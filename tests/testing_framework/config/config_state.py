271978fe9fda20f623fe407281c7cbcc083d5b34
Alessandro Fanio González <alessandro.fg.27gmail.com>
2013-07-11 14:22:26 +0100

    OWTF 0.30 "Summer Storm II": Added some new features to the Unit Testing Framework.

---
+++ tests/testing_framework/config/config_state.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+import copy
+from hamcrest.library.collection.isin import IsIn
+
+class ConfigState():
+
+    def __init__(self, config_obj):
+        self.Config = deepcopy(config_obj.Config)
+        self.TargetConfig = deepcopy(config_obj.TargetConfig)
+        self.Targets = deepcopy(config_obj.Targets)
+
+    @staticmethod
+    def restore_state(config_obj, state_obj):
+        config_obj.Config = deepcopy(state_obj.Config)
+        config_obj.TargetConfig = deepcopy(config_obj.TargetConfig)
+        config_obj.Targets = deepcopy(config_obj.Targets)
+
+
+def deepcopy(obj):
+    if isinstance(obj, defaultdict) or isinstance(obj, dict):
+        obj_copy = defaultdict(list)
+        for key, value in obj.items():
+            obj_copy[key] = deepcopy(value)
+        return obj_copy
+    else:
+        return copy.deepcopy(obj)
