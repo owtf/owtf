02d5b0b1eec0f27ab3f4955f893a4d75126bc296
Alessandro Fanio González <alessandro.fg.27@gmail.com>
2013-09-22 19:31:59 +0200

    OWTF 0.40 'Summer Storm III': Added comments and documentation to the testing framework features.

--- tests/testing_framework/base_test_cases.py
+++ tests/testing_framework/base_test_cases.py
@@ -1,24 +1,22 @@
-import unittest
-import sys
-import re
 from io import BytesIO as StringIO
 from os import path
 from tests.testing_framework.server import HandlerBuilder, WebServerProcess
-import shlex
-from owtf import ProcessOptions
-from framework.core import Core
-import os
 from tests.testing_framework.utils import ExpensiveResourceProxy
-from tests.testing_framework.doubles.mock import StreamMock
-import shutil
 from hamcrest.library.text.stringcontains import contains_string
 from hamcrest.core.assert_that import assert_that
-import string
-import random
 from hamcrest.core.core.isequal import equal_to
-import cgi
 from hamcrest.library.text.stringmatches import matches_regexp
 from hamcrest.library.number.ordering_comparison import greater_than_or_equal_to
+from owtf import ProcessOptions
+from framework.core import Core
+import unittest
+import sys
+import re
+import shutil
+import string
+import random
+import shlex
+import os
 
 
 class BaseTestCase(unittest.TestCase):
@@ -38,10 +36,12 @@ class BaseTestCase(unittest.TestCase):
             pass  # The subclass does not implement the tear down method
 
     def init_stdout_recording(self):
+        """Replaces the system stdout with a string buffer."""
         self.stdout_backup = sys.stdout
         self.replace_stdout_with_string_buffer()
 
     def get_recorded_stdout(self, flush_buffer=False):
+        """Gets the content of the stream buffer, and, optionally, flush it."""
         output = self.stdout_content.getvalue()
         if (flush_buffer):
             self.stdout_content.close()
@@ -53,6 +53,7 @@ class BaseTestCase(unittest.TestCase):
         sys.stdout = self.stdout_content
 
     def stop_stdout_recording(self):
+        """Close the string buffer, and restore the system stdout."""
         self.stdout_content.close()
         sys.stdout = self.stdout_backup
 
@@ -62,9 +63,11 @@ class BaseTestCase(unittest.TestCase):
         return output
 
     def get_resource_path(self, relative_path):
+        """Shortcut to retrieve the absolute path of a test resource."""
         return self.get_abs_path(self.RESOURCES_DIR + relative_path)
 
     def get_abs_path(self, relative_path):
+        """Shortcut to retrieve the absolute path of a file"""
         return path.abspath(relative_path)
 
 
@@ -83,7 +86,7 @@ class CoreInitialiser():
         self.core_instance = Core(root_dir)
         processed_options = self.process_options(options)
         self.core_instance.initialise_framework(processed_options)
-        self.core_instance.PluginHandler.CanPluginRun = lambda arg1, arg2: True
+        self.core_instance.PluginHandler.CanPluginRun = lambda arg1, arg2: True  # In tests, we always want to run the plugins
         return self.core_instance
 
     def process_options(self, options):
@@ -99,17 +102,21 @@ class PluginTestCase(BaseTestCase):
     OWTF_REVIEW_FOLDER = "owtf_review"
     LOG_FILE = "logfile"
     NOT_INTERACTIVE = " -i no "
+    BUG_FOUND_BANNER = "OWTF BUG: Please report the sanitised information below to help make this better. Thank you."
+    BUG_FOUND_MESSAGE = "The test failed because a bug was found during the execution."
 
     core_instance_proxy = None
 
     @classmethod
     def setUpClass(cls):
+        """Prepares the OWTF instance, and clean the possible existing temp files"""
         cls.clean_temp_files()  # Cleans logfile and owtf_review/ before starting
         cls.core_instance_proxy = ExpensiveResourceProxy(CoreInitialiser("http://" + cls.TARGET))
         super(PluginTestCase, cls).setUpClass()
 
     @classmethod
     def tearDownClass(cls):
+        """Finish the OWTF instance correctly, and clean temp files."""
         try:
             if cls.core_instance_can_be_destroyed():
                 core = cls.core_instance_proxy.get_instance()
@@ -134,6 +141,7 @@ class PluginTestCase(BaseTestCase):
         return core_instance_proxy_is_defined and is_not_none
 
     def setUp(self):
+        """Restart the instance variables for each test"""
         super(BaseTestCase, self).setUp()
         self.core_instance = self.core_instance_proxy.get_instance()
         self.owtf_output = ""
@@ -183,24 +191,27 @@ class PluginTestCase(BaseTestCase):
         return result
 
     def check_for_bugs(self):
-        bug_found_banner = "OWTF BUG: Please report the sanitised information below to help make this better. Thank you."
-        if self.owtf_output.count(bug_found_banner) > 0:
+        if self.owtf_output.count(self.BUG_FOUND_BANNER) > 0:
             print self.owtf_output
-            self.fail("The test failed because a bug was found during the execution.")
+            self.fail(self.BUG_FOUND_MESSAGE)
 
     def assert_external_tool_started(self, times):
+        """Assert that the plugin started a given number of tools."""
         self.assert_that_output_contains(self.PLUGIN_START_TEXT, times)
 
     def assert_that_output_contains_lines(self, lines):
+        """Assert that the output of the plugin contains a set of strings."""
         for line in lines:
             self.assert_that_output_contains(line)
 
     def assert_that_output_contains(self, substring, times=None):
+        """Assert that the output of the plugin contains the given content."""
         assert_that(self.owtf_output, contains_string(substring))
         if times is not None:
             assert_that(self.owtf_output.count(substring), equal_to(times))
 
     def assert_that_output_matches_more_than(self, regex, times):
+        """Assert that the output matches a given regular expression a specified number of times."""
         assert_that(self.owtf_output, matches_regexp(regex))
         if times is not None:
             times_replaced = self._get_number_of_occurences_for(regex)
@@ -221,6 +232,7 @@ class PluginTestCase(BaseTestCase):
         return ''.join(choices)
 
     def get_resources(self, resource_name):
+        """Shortcut to the Config.GetResources method."""
         return self.core_instance.Config.GetResources(resource_name)
 
 
@@ -326,6 +338,7 @@ class WebPluginTestCase(PluginTestCase):
         self.server.stop()
 
     def check_link_generation_for_resources(self, resources_name):
+        """Check that the given resources are correctly linked in the output of the plugin."""
         if not isinstance(resources_name, list):
             resources_name = [resources_name]
         for resource in resources_name:
@@ -334,6 +347,7 @@ class WebPluginTestCase(PluginTestCase):
             self.assert_that_output_matches_more_than(self.PASSIVE_LINK_PATTERN, times)
 
     def get_url(self, web_path, https=False):
+        """Get an absolute URL from a relative URL."""
         protocol = "https" if https else "http"
         return protocol + "://" + self.TARGET + web_path
 
@@ -346,6 +360,10 @@ class WebPluginTestCase(PluginTestCase):
         self.core_instance.Requester.GetTransaction(True, url, Method=method)
 
     def generate_headers_with_tokens(self, header_names):
+        """
+            Generates a dictionary using the header_names as keys and random strings as
+            values.
+        """
         headers = {}
         for name in header_names:
             headers[name] = self.generate_random_token()
--- tests/testing_framework/utils.py
+++ tests/testing_framework/utils.py
@@ -1,6 +1,11 @@
 
 
 class ExpensiveResourceProxy():
+    """
+        Class used to cache an object with a time-expensive initialization using
+        a proxy pattern. It receives a callable that will instantiate the
+        expensive object and use it when it is required.
+    """
 
     def __init__(self, init_function):
         self.init_function = init_function
