9bb31a26d5da8697f190a7fbee3b543026d8a0d0
Alessandro Fanio González <alessandro.fg.27@gmail.com>
2013-10-12 17:35:24 +0200

    OWTF 0.40 'Summer Storm III': Fixed #75

--- owtf.py
+++ owtf.py
@@ -123,7 +123,7 @@ def GetArgs(Core, args):
                         help="<wave1,wave2,wave3> - Waves to run network scanning")
     Parser.add_argument("-proxy", "--proxy",
                         dest="ProxyMode",
-                        default=True,
+                        default=False,
                         action="store_true",
                         help="Use this flag to run OWTF Inbound Proxy")
     Parser.add_argument("--update", "--update",
