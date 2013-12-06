bfc65a925a8bcdf073f4f3a3d61a86e4f2fbebed
7a <abraham.aranguren@owasp.org>
2013-12-06 20:59:43 +0100

    OWTF 0.40 'Summer Storm III': Removing -useproxy from nikto until we test that better since it makes nikto say that 'No web server found on...' which I verified NOT to be the case. Without -useproxy nikto works but obviously the proxy won't see the HTTP transactions, so this is not a fix of the problem

--- scripts/run_nikto.sh
+++ scripts/run_nikto.sh
@@ -66,12 +66,14 @@ fi
 
 # Temporary nikto config file
 NIKTO_CONF_FILE="/etc/nikto.conf"
-TEMP_NIKTO_CONF_FILE="/tmp/temp_owtf_nikto.conf"
+# Abe: Adding pid to temporary config to avoid potential concurrent process issues
+PID=$$
+TEMP_NIKTO_CONF_FILE="/tmp/temp_owtf.$PID.nikto.conf"
 if [ -f $TEMP_NIKTO_CONF_FILE ]; then
     rm $TEMP_NIKTO_CONF_FILE
 fi
 cp $NIKTO_CONF_FILE $TEMP_NIKTO_CONF_FILE
-echo "PROXYHOST=$PROXY_IP" >> $TEMP_NIKTO_CONF_FILE
+#echo "PROXYHOST=$PROXY_IP" >> $TEMP_NIKTO_CONF_FILE
 echo "PROXYPORT=$PROXY_PORT" >> $TEMP_NIKTO_CONF_FILE
 
 DATE=$(date +%F_%R:%S | sed 's/:/_/g')
@@ -79,11 +81,14 @@ OUTFILE="nikto$DATE"
 LOG_XML=$OUTFILE.xml
 DIR=$(pwd) # Remember current dir
 cd "$TOOL_DIR" # Nikto needs to be run from its own folder
+echo $TOOL_DIR
 if [ ! -f ./nikto.pl ]; then # Kali linux hack
 	cp /usr/bin/nikto /usr/share/nikto/nikto.pl # Ensure the executable is on the right place, so that below runs even if the user did not run the install script
 fi
 # There is a import error when running from Nikto dir i.e /usr/share/nikto
-COMMAND="nikto $NIKTO_NOLOOKUP -evasion 1 $NIKTO_SSL -config $TEMP_NIKTO_CONF_FILE  -useproxy -host $HOST_NAME -port $PORT -output $DIR/$LOG_XML -Format xml"
+# Abe: somehow the -useproxy option makes nikto abort with '+ No web server found on', removing "-useproxy" while we figure that out
+#COMMAND="nikto $NIKTO_NOLOOKUP -evasion 1 $NIKTO_SSL -config $TEMP_NIKTO_CONF_FILE  -useproxy -host $HOST_NAME -port $PORT -output $DIR/$LOG_XML -Format xml"
+COMMAND="nikto $NIKTO_NOLOOKUP -evasion 1 $NIKTO_SSL -config $TEMP_NIKTO_CONF_FILE -host $HOST_NAME -port $PORT -output $DIR/$LOG_XML -Format xml"
 echo "[*] Running: $COMMAND"
 $COMMAND
 
