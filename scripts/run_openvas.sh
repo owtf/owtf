548efd650c9004699bdc93cfab6fa968b563f3e6
dscarson <dscarosn@live.com>
2014-01-01 19:13:40 -0800

    OpenVAS script

--- scripts/run_openvas.sh
+++ scripts/run_openvas.sh
@@ -5,7 +5,7 @@
 # owtf is an OWASP+PTES-focused try to unite great tools and facilitate pen testing
 # Copyright (c) 2011, Abraham Aranguren <name.surname@gmail.com> Twitter: @7a_ http://7-a.org
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 # * Redistributions of source code must retain the above copyright 
@@ -27,7 +27,8 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+
+
 
 
 if [ $# -ne 3 -a $# -ne 4 ]; then
@@ -44,25 +45,30 @@ if [ $2 ]; then
 	USER_AGENT=$(echo $3 | sed 's/#/ /g') # Expand to real User Agent
 fi
 
-echo $URL
+
 
 DATE=$(date +%F_%R_%S | sed 's/:/_/g')
 echo "DATE=$DATE"
-OUTFILE="openvas_report$DATE"
+OUTFILE="OpenVAS_Main_Report_$DATE"
 DIR=$(pwd) # Remember current dir
 echo
 PORT=$(netstat -evantupo|grep openvas|grep LISTEN| sed 's/  */#/g'|cut -f4 -d# | cut -f2 -d:)
+
+#Port-check doesn't always work as openvassd is sometimes the single process running (without openvasmd and openvasad)
+#same thing happens for gsad, so it is better to check for each one
 
 if [ "$PORT" = "" ]; then
    #GSAD=$(netstat -evantupo|grep gsad|grep LISTEN| sed 's/  */#/g' | cut -f9 -d'#' | cut -f1 -d'/')
    #if [ "$GSAD" != "" ];then 
    #   kill -9 $GSAD
    #fi
-   pkill -9 openvas
-   pkill -9 gsad
    
+   
+   pkill -9 gsad
+   sleep 1
    echo "Starting OpenVas Services"
-   openvas-nvt-sync
+   #this will take time
+   openvas-nvt-sync  #this prints output in weird format,same issue as above (progress status)
    openvas-scapdata-sync
    openvas-certdata-sync
    openvasmd --rebuild
@@ -73,6 +79,25 @@ if [ "$PORT" = "" ]; then
    gsad --http-only --listen=127.0.0.1 -p 9392
    
    sleep 10
+
+else 
+     if [ "$(netstat -evantupo|grep openvassd|grep LISTEN| sed 's/  */#/g'|cut -f4 -d# | cut -f2 -d:)" = ""  ]; then 
+        
+        openvassd
+     fi
+     if [ "$(netstat -evantupo|grep openvasmd|grep LISTEN| sed 's/  */#/g'|cut -f4 -d# | cut -f2 -d:)" = ""  ]; then
+       
+        openvasmd
+     fi
+     if [ "$(netstat -evantupo|grep openvasmd|grep LISTEN| sed 's/  */#/g'|cut -f4 -d# | cut -f2 -d:)" = "" ]; then
+        openvasad
+     fi
+     if [ "$(netstat -evantupo|grep gsad|grep LISTEN| sed 's/  */#/g'|cut -f4 -d# | cut -f2 -d:)" = "" ];then
+        
+        gsad --http-only --listen=127.0.0.1 -p 9392
+        sleep 1
+     fi
+
 fi
 
 echo "Runnig OpenVAS Plugin.."
@@ -94,33 +119,32 @@ do
   fi   
 #echo $TARGET_ID
 done
-echo "#####################################################################"
-
-echo "###                                                               ### 
-###                  __  __  __            __  __                 ###
-###                 |  ||__||__ |\ | \  / |__||__                 ###
-###                 |__||   |__ | \|  \/  |  | __|                ###
-###                                                               ###
+echo "#########################################################################"
+echo "###                                                                   ### 
+###                      __  __  __            __  __                 ###
+###                     |  ||__||__ |\ | \  / |__||__                 ###
+###                     |__||   |__ | \|  \/  |  | __|                ###
+###                                                                   ###
 ### "
 
 echo "###--------------Target Created : OWTF_Target_$URL..."
-
+#Default Fast and full Scan
 CONFIG_ID="daba56c8-73ec-11df-a475-002264764cea" #by-default Fast and full scan
 
 TASK_ID=$(omp -u admin -w $PASS --xml="<create_task><name>OWTF_Task_$URL</name>
                                        <config id=\"$CONFIG_ID\"/>
                                        <target id=\"$TARGET_ID\"/>
                     </create_task>" |  sed 's/  *//g'|cut -f2 -d'"')
-echo "###---------------------------------------------------------------###"
+echo "###-------------------------------------------------------------------###"
 echo "###--------------Task Created : OWTF_Task_$URL..."
 
 REPORT_ID=$(omp -u admin -w $PASS --xml="<start_task task_id=\"$TASK_ID\"/>" | sed 's/  *//g'|cut -f3 -d'>' |cut -f1 -d'<')
-echo "###---------------------------------------------------------------###"
-echo "###--------------Task Started-------------------------------------###"
+echo "###-------------------------------------------------------------------###"
+echo "###--------------Task Started-----------------------------------------###"
 
-echo "###---------------------------------------------------------------###"
+echo "###-------------------------------------------------------------------###"
 
-echo "###--------------Status Check-------------------------------------###"
+echo "###--------------Status Check-----------------------------------------###"
 echo -e "\n"
 
 
@@ -129,11 +153,14 @@ echo -e "\n"
 
 STATUS=$(omp -u admin -w $PASS -G | grep $TASK_ID|sed 's/  */#/g'|cut -f2,3 -d'#')
 #STATUS="NO"
-echo "In Progress..."
+echo "In Progress...Hang tight !!"
+echo "(You can check your status of progress by going to http://127.0.0.1:9392 and logging in
+with the username 'admin' and the password which you just entered and then going to tasks tab in scan management)".
 while [[ $STATUS != *Done* ]]
 do
-   
-   #echo -ne "One Moment please $sek ... \r"
+   #All the below statements were for progress bar.But as that is not yet possible in OWTF,
+   #I have kept it for future use.
+  
    #tput el1
    #tput rc
    #echo -n "###"
@@ -147,12 +174,11 @@ do
    if [[ $STATUS = *Stopped* ]];then
      break
    fi
-   #STATUS="Done"
-  
 done
-omp -u admin -w $PASS --delete-task $TASK_ID
 
-echo -n "###------------------Done !---------------------------------------###"
+omp -u admin -w $PASS --delete-task $TASK_ID
+echo -e "\n"
+echo -n "###------------------Done !-------------------------------------------###"
 echo -e "\n"
 
 
@@ -179,16 +205,16 @@ echo -e "\n"
 
 
 
-echo "###---------------------------------------------------------------###"
-echo "###--------------Status Check Complete----------------------------###"
-echo "###---------------------------------------------------------------###"
+echo "###-------------------------------------------------------------------###"
+echo "###--------------Status Check Complete--------------------------------###"
+echo "###-------------------------------------------------------------------###"
 
 DIR=$(pwd)
 echo "###--------------Creating report in $DIR"...
 omp -u admin -w napster --get-report $REPORT_ID  --format 6c248850-1f62-11e1-b082-406186ea4fc5  > $OUTFILE.html
-echo "###---------------------------------------------------------------###"
-echo "###--------------Report Generated---------------------------------###"
+echo "###-------------------------------------------------------------------###"
+echo "###--------------Report Generated-------------------------------------###"
 echo -e "\n"
 
-echo "###----------------- [*] Done!] --------------------------------- ###"
-echo "#####################################################################"
+echo "###----------------- [*] Done!] --------------------------------------###"
+echo "#########################################################################"
