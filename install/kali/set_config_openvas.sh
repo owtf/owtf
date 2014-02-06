0f21115ccbce54420cb4fe74f075cf9b190332ac
dscarson <dscarosn@live.com>
2014-02-06 12:24:46 -0800

    Script updated

--- install/kali/set_config_openvas.sh
+++ install/kali/set_config_openvas.sh
@@ -1,55 +1,68 @@
 #!/usr/bin/env bash
 
+. $(pwd)/../../install/kali/openvas_init.sh
 
-echo -n "Enter the port for gsad web interface, default is 9392 (Use default (Y/N)):"
-read ch
-if  ([[ $ch = "y" ]]  || [[ $ch = "Y" ]] || [[ -z $ch ]])
-then 
- echo $ch
- port=9392
-else 
- echo -n "Enter the port :"
- read port
+if [[ "$port" = "" ]]
+then
+  port=9392
+  update_config_setting "OPENVAS_GSAD_PORT" "9392"
 fi
-dir=$(pwd)
-dir1="$dir/../../profiles/general/default.cfg"
-sed -i '/OPENVAS_GSAD_PORT/d' $dir1
-echo "OPENVAS_GSAD_PORT: "$port >> $dir1
 
-read -s -p "Enter password for admin(cannot be blank) : `echo  $'\n '`Password :" PASS
-sed -i '/OPENVAS_PASS/d' $dir1
-echo "OPENVAS_PASS: "$PASS >> $dir1
+choice_passwd_change="n"
+if [[ "$passwd" = "" ]]
+then
+  choice="y"
+else
+
+  echo -n "Admin user already exists, want to overwrite ( existing user will be deleted ) [y/n]?"
+  read choice
+fi
+
+if  ([[ $choice = "y" ]]  || [[ $choice = "Y" ]] || [[ -z $choice ]])
+then
+  read -s -p "Enter password for admin ( cannot be blank ) : `echo  $'\n '`Password :" passwd
+  update_config_setting "OPENVAS_PASS" "$passwd"
+  choice_passwd_change="y"
+fi
+
 echo
 echo
 
-echo -e "Enter the scan configuration type \n1)Full and Fast \n2)Full and fast ultimate \n3)Full and very deep \n4)Full and very deep ultimate:"
-
-flag=1
-while [[ $flag -eq 1 ]]
-do
-echo -n "Enter your choice (by default it's Full and Fast ) :"
-read ch
-flag=0
-if  ([[ $ch = "1" ]]  || [[ -z $ch ]])
+if [[ "$config_id" = "" ]]
 then 
- configid="daba56c8-73ec-11df-a475-002264764cea"
-elif [[ $ch = "2" ]]
-then
- configid="698f691e-7489-11df-9d8c-002264764cea"
-elif [[ $ch = "3" ]]
-then
- configid="708f25c4-7489-11df-8094-002264764cea"
-elif [[ $ch = "4" ]]
-then
- configid="74db13d6-7489-11df-91b9-002264764cea"
-else 
- flag=1
+ choice="y"
+else
+ echo -n "Scan type is already defined.Want to change (This will overwrite the previous scan config) ?[y/n] :"
+ read choice
 fi
-done
 
-sed -i '/OPENVAS_CONFIG_ID/d' $dir1
-echo "OPENVAS_CONFIG_ID: "$configid >> $dir1
-
+if  ([[ $choice = "y" ]]  || [[ $choice = "Y" ]] || [[ -z $choice ]])
+then
+  echo -e "Enter the scan configuration type \n1)Full and Fast \n2)Full and fast ultimate \n3)Full and very deep \n4)Full and very deep ultimate:"
+  flag=1
+  while [[ $flag -eq 1 ]]
+  do
+    echo -n "Enter your choice (by default it's Full and Fast ) :"
+    read config_choice
+    flag=0
+    if  ([[ $config_choice = "1" ]]  || [[ -z $config_choice ]])
+    then 
+       configid="daba56c8-73ec-11df-a475-002264764cea"
+    elif [[ $config_choice = "2" ]]
+    then
+       configid="698f691e-7489-11df-9d8c-002264764cea"
+    elif [[ $config_choice = "3" ]]
+    then
+       configid="708f25c4-7489-11df-8094-002264764cea"
+    elif [[ $config_choice = "4" ]]
+    then
+       configid="74db13d6-7489-11df-91b9-002264764cea"
+    else 
+       flag=1
+    fi
+  done
+  update_config_setting "OPENVAS_CONFIG_ID" "$configid"
+fi
 
 
 
