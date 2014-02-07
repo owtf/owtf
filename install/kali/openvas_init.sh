dbba3e4d033b5b58401418532695308cf88ab279
dscarson <dscarosn@live.com>
2014-02-07 10:42:33 -0800

    Path changes and variable name changes made

--- install/kali/openvas_init.sh
+++ install/kali/openvas_init.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-export CUR_DIR=$(pwd)
-export CONFIG_FILE="$CUR_DIR/../../profiles/general/default.cfg"
-export port=$(grep OPENVAS_GSAD_PORT $CONFIG_FILE | cut -f2 -d' ')
-export passwd=$(grep OPENVAS_PASS $CONFIG_FILE | cut -f2 -d' ')
-export config_id=$(grep OPENVAS_CONFIG_ID $CONFIG_FILE | cut -f2 -d' ')
+OWTF_RootDir=$1
+OWTF_CONFIG_FILE="$OWTF_RootDir/profiles/general/default.cfg"
 
 update_config_setting(){
-
-  sed -i '/$1/d' $CONFIG_FILE
-  echo "$1: "$2>> $CONFIG_FILE 
+  
+  eval sed -i '/"$1"/d' $OWTF_CONFIG_FILE 
+  echo "$1: "$2>> $OWTF_CONFIG_FILE 
 
 }
+
+get_config_setting(){
+  grep $1 $OWTF_CONFIG_FILE | cut -f2 -d' '
+}
+
+OWTF_PGSAD=$(get_config_setting "OPENVAS_GSAD_PORT" )
+OWTF_GSAD_IP=$(get_config_setting "OPENVAS_GSAD_IP")
+OWTF_OPENVAS_PASSWD=$(get_config_setting "OPENVAS_PASS")
+OWTF_CONFIG_ID=$(get_config_setting "OPENVAS_CONFIG_ID")
