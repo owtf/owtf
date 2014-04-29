98d9939f360eaa4c0eb5f81b020d1a8ec233743c
Tao Sauvage <sauvage.tao@gmail.com>
2014-04-29 22:42:21 +0200

    [plugin] Fix typo

--- plugins/web/active/Visit_URLs@OWTF-WSP-001.py
+++ plugins/web/active/Visit_URLs@OWTF-WSP-001.py
@@ -39,4 +39,4 @@ def run(Core, PluginInfo):
         Core.Requester.GetTransaction(True, url) # Use cache if possible
     Content = str(len(urls))+" URLs were visited"
     Core.log(Content)
-    return([{"type":"Html", "output":{"HtmlString":Content}])
+    return([{"type":"Html", "output":{"HtmlString":Content}}])
