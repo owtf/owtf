fcb54b51774343fadb85927da83f35dd5803c06c
Tao Sauvage <sauvage.tao@gmail.com>
2014-05-22 18:56:03 +0200

    Fix - Automatically accept w3af's terms and conditions if run for the first time (#225)

--- scripts/run_w3af.sh
+++ scripts/run_w3af.sh
@@ -33,6 +33,23 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+if [ -f ~/.w3af/startup.conf ]
+then
+    if ! grep -i "^accepted-disclaimer = true$" ~/.w3af/startup.conf
+    then
+        echo "accepted-disclaimer = true" >> ~/.w3af/startup.conf
+    fi
+else
+    if [ ! -d ~/.w3af ]
+    then
+        mkdir ~/.w3af
+    fi
+    echo "[STARTUP_CONFIG]" >> ~/.w3af/startup.conf
+    echo "auto-update = true" >> ~/.w3af/startup.conf
+    echo "frequency = D" >> ~/.w3af/startup.conf
+    echo "accepted-disclaimer = true" >> ~/.w3af/startup.conf
+fi
+
 if [ $# -ne 3 -a $# -ne 4 ]; then
         echo "Usage $0 <tool_dir> <target url> <proxy> (<user agent -spaces replaced by # symbol->)"
         exit
