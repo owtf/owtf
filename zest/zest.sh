fc6ecf6a36737efc20c847928c1b5e5707feb2b6
dscarson <dscarosn@live.com>
2014-05-22 23:05:32 -0700

    [zest]zest script creator module

---
+++ zest/zest.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+<<COMM
+echo "request header -  $2"
+echo "rresponse header - $3"
+echo "response body -   $4"
+COMM
+
+
+
+java -jar $5/zest/zest.jar "$1" "$2" "$3" "$4" "$5"
+
