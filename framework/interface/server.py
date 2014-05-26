fb5bc58fd4090e9b111ea371a82fcfb6fa455ec8
Tao Sauvage <sauvage.tao@gmail.com>
2014-05-26 10:39:05 +0200

    PEP8 on server

--- framework/interface/server.py
+++ framework/interface/server.py
@@ -7,24 +7,34 @@ import tornado.options
 class InterfaceServer(object):
     def __init__(self, Core):
         self.application = tornado.web.Application(
-                                                    handlers=urls.get_handlers(Core),
-                                                    template_path=Core.Config.FrameworkConfigGet('INTERFACE_TEMPLATES_DIR'),
-                                                    debug=False,
-                                                    gzip=True,
-                                                    compiled_template_cache=False
-                                                  )
+            handlers=urls.get_handlers(Core),
+            template_path=Core.Config.FrameworkConfigGet(
+                'INTERFACE_TEMPLATES_DIR'),
+            debug=False,
+            gzip=True,
+            compiled_template_cache=False
+            )
         self.application.Core = Core
         self.server = tornado.httpserver.HTTPServer(self.application)
         # The next line is the heart of everything
-        self.manager_cron = tornado.ioloop.PeriodicCallback(self.application.Core.WorkerManager.manage_workers, 2000)
+        self.manager_cron = tornado.ioloop.PeriodicCallback(
+            self.application.Core.WorkerManager.manage_workers,
+            2000)
 
     def start(self):
         try:
             self.server.bind(
-                int(self.application.Core.Config.FrameworkConfigGet("UI_SERVER_PORT")),
-                address=self.application.Core.Config.FrameworkConfigGet("UI_SERVER_ADDR")
-            )
-            tornado.options.parse_command_line(args=["dummy_arg","--log_file_prefix=/tmp/ui.log","--logging=info"])
+                int(self.application.Core.Config.FrameworkConfigGet(
+                    "UI_SERVER_PORT")),
+                address=self.application.Core.Config.FrameworkConfigGet(
+                    "UI_SERVER_ADDR")
+                )
+            tornado.options.parse_command_line(
+                args=[
+                    'dummy_arg',
+                    '--log_file_prefix=/tmp/ui.log',
+                    '--logging=info']
+                )
             self.server.start(1)
             self.manager_cron.start()
             tornado.ioloop.IOLoop.instance().start()
