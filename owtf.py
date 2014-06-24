4bf1d6c1053894c0b669be03a54d093a95b49f4b
Tao Sauvage <sauvage.tao@gmail.com>
2014-06-24 20:39:36 +0200

    [owtf] Revert previous permissions checking function.
    Reverted commits:
    
    + 5c410e9def6985daa3928037d510104bae373836
    + 7aef0248d7b9f8c157279faad360482a6ff8c572
    + 3157669da1b4dfe331f2b146f4879244912a7dec
    + 71df07b018a218b2fbde441f7b10d9ab1fd0a601

--- owtf.py
+++ owtf.py
@@ -59,61 +59,6 @@ def banner():
 """)
 
 
-def Permissions():
-    """
-    This function checks for writing privileges into /tmp/owtf and /owtf_review recursively
-    /tmp is an OS directory
-    /owtf_review is stored in OWTF directory
-    """
-    message = "Error: Not having enough privileges to write into: "
-    tmpDirPath = '/tmp/owtf'
-    reviewDirPath = RootDir + '/owtf_review'
-
-    if os.path.exists(tmpDirPath):
-        tmp_owtf = os.access(tmpDirPath, os.W_OK)
-    else:
-        tmp_owtf = True
-        try:
-            os.mkdir(tmpDirPath)
-        except OSError as e:
-            cprint(e.strerror)
-            sys.exit(-1)
-
-    if os.path.exists(reviewDirPath):
-        perm_error = False
-        if os.access(reviewDirPath, os.W_OK):
-            for root, dirs, files in os.walk(reviewDirPath):
-                for dir_ in dirs:
-                    dir_path = os.path.join(root, dir_)
-                    #print(dir_path)
-                    if not os.access(dir_path, os.W_OK):
-                        cprint(message + dir_path)
-                        perm_error = True
-                for file_ in files:
-                    file_path = os.path.join(root, file_)
-                    if not os.access(file_path, os.W_OK):
-                        perm_error = True
-                        cprint(message + os.path.join(root, file_))
-        else:
-            perm_error = True
-            cprint(message + reviewDirPath)
-
-        if perm_error:
-            sys.exit(-1)
-
-    else:
-
-        try:
-            os.mkdir(reviewDirPath)
-        except OSError as e:
-            cprint(e.strerror)
-            exit(-1)
-
-    if not tmp_owtf:
-        cprint(message + tmpDirPath)
-        sys.exit(1)
-
-
 def GetArgs(Core, args):
     ValidPluginGroups = Core.DB.Plugin.GetAllGroups()
     ValidPluginTypes = Core.DB.Plugin.GetAllTypes() + ['all', 'quiet']
@@ -460,7 +405,6 @@ def run_owtf(Core, args):
 
 if __name__ == "__main__":
     banner()
-    Permissions()
     if not "--update" in sys.argv[1:]:
         Core = core.Init(RootDir, OwtfPid)  # Initialise Framework
         print(
