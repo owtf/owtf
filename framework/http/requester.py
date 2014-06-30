8790cb1c08b26b7021487936141010d05a844869
Tao Sauvage <sauvage.tao@gmail.com>
2014-06-30 21:25:53 +0200

    [requester] PEP8 misc.

--- framework/http/requester.py
+++ framework/http/requester.py
@@ -43,69 +43,97 @@ from framework.http import transaction
 from framework.lib.general import *
 
 
-# Intercept raw request trick from: http://stackoverflow.com/questions/6085709/get-headers-sent-in-urllib2-http-request
+# Intercept raw request trick from:
+# http://stackoverflow.com/questions/6085709/get-headers-sent-in-urllib2-http-request
 class MyHTTPConnection(httplib.HTTPConnection):
     def send(self, s):
         global RawRequest
-        RawRequest.append(s) # Saving to global variable for Requester class to see
+        # Saving to global variable for Requester class to see.
+        RawRequest.append(s)
         httplib.HTTPConnection.send(self, s)
+
 
 class MyHTTPHandler(urllib2.HTTPHandler):
     def http_open(self, req):
         try:
             return self.do_open(MyHTTPConnection, req)
         except KeyboardInterrupt:
-            raise KeyboardInterrupt # Not handled here
+            raise KeyboardInterrupt  # Not handled here.
         except Exception, e:
-            return '' # Can't have OWTF crash due to a library exception -i.e. raise BadStatusLine(line)-
+            # Can't have OWTF crash due to a library exception -i.e. raise
+            # BadStatusLine(line)-
+            return ''
+
 
 class MyHTTPSConnection(httplib.HTTPSConnection):
     def send(self, s):
         global RawRequest
-        RawRequest.append(s) # Saving to global variable for Requester class to see
+        # Saving to global variable for Requester class to see.
+        RawRequest.append(s)
         httplib.HTTPSConnection.send(self, s)
+
 
 class MyHTTPSHandler(urllib2.HTTPSHandler):
     def https_open(self, req):
         try:
             return self.do_open(MyHTTPSConnection, req)
         except KeyboardInterrupt:
-            raise KeyboardInterrupt # Not handled here
+            raise KeyboardInterrupt  # Not handled here.
         except Exception, e:
-            return '' # Can't have OWTF crash due to a library exception -i.e. raise BadStatusLine(line)-
+            # Can't have OWTF crash due to a library exception -i.e. raise
+            # BadStatusLine(line)-.
+            return ''
 
-# SmartRedirectHandler is courtesy of: http://www.diveintopython.net/http_web_services/redirects.html
+
+# SmartRedirectHandler is courtesy of:
+# http://www.diveintopython.net/http_web_services/redirects.html
 class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_301(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
+        result = urllib2.HTTPRedirectHandler.http_error_301(
+            self, req, fp, code, msg, headers)
         result.status = code
         return result
 
     def http_error_302(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        result = urllib2.HTTPRedirectHandler.http_error_302(
+            self, req, fp, code, msg, headers)
         result.status = code
         return result
+
 
 class Requester:
     def __init__(self, Core, Proxy):
         self.Core = Core
-        self.Headers = { 'User-Agent' : self.Core.DB.Config.Get('USER_AGENT') }
+        self.Headers = {'User-Agent': self.Core.DB.Config.Get('USER_AGENT')}
         self.RequestCountRefused = 0
         self.RequestCountTotal = 0
         self.LogTransactions = False
         self.Proxy = Proxy
-        if None == Proxy:
-            cprint("WARNING: No outbound proxy selected. It is recommended to use an outbound proxy for tactical fuzzing later")
-            self.Opener = urllib2.build_opener(MyHTTPHandler, MyHTTPSHandler, SmartRedirectHandler) # "Smart" redirect handler not really working
-        else: # All requests must use the outbound proxy
+        if Proxy is None:
+            cprint(
+                "WARNING: No outbound proxy selected. It is recommended to "
+                "use an outbound proxy for tactical fuzzing later")
+            self.Opener = urllib2.build_opener(
+                MyHTTPHandler,
+                MyHTTPSHandler,
+                # FIXME: "Smart" redirect handler not really working.
+                SmartRedirectHandler)
+        else:  # All requests must use the outbound proxy.
             cprint("Setting up proxy(inbound) for OWTF requests..")
             IP, Port = Proxy
-            ProxyConf = { 'http':'http://'+IP+":"+Port, 'https':'http://'+IP+":"+Port }
+            ProxyConf = {
+                'http': 'http://' + IP + ":" + Port,
+                'https': 'http://' + IP + ":" + Port}
             ProxyHandler = urllib2.ProxyHandler(ProxyConf)
-            self.Opener = urllib2.build_opener(ProxyHandler, MyHTTPHandler, MyHTTPSHandler, SmartRedirectHandler) # works except no raw request on https
+            self.Opener = urllib2.build_opener(
+                ProxyHandler,
+                MyHTTPHandler,
+                MyHTTPSHandler,
+                # FIXME: Works except no raw request on https.
+                SmartRedirectHandler)
         urllib2.install_opener(self.Opener)
 
-    def log_transactions(self, LogTransactions = True):
+    def log_transactions(self, LogTransactions=True):
         Backup = self.LogTransactions
         self.LogTransactions = LogTransactions
         return Backup
@@ -114,13 +142,16 @@ class Requester:
         return not self.Core.PluginHandler.NormalRequestsAllowed()
 
     def IsTransactionAlreadyAdded(self, URL):
-        return self.Core.DB.Transaction.IsTransactionAlreadyAdded({'url': URL.strip()})
+        return self.Core.DB.Transaction.IsTransactionAlreadyAdded({
+            'url': URL.strip()})
 
     def is_request_possible(self):
         return self.Core.PluginHandler.RequestsPossible()
 
     def ProxyCheck(self):
-        if self.Proxy != None and self.is_request_possible(): # Verify proxy works! www.google.com might not work in a restricted network, try target URL :)
+        # Verify proxy works! www.google.com might not work in a restricted
+        # network, try target URL :)
+        if self.Proxy != None and self.is_request_possible():
             URL = self.Core.DB.Config.Get('PROXY_CHECK_URL')
             #if self.NeedToAskBeforeRequest() and 'y' != raw_input("Proxy Check: Need to send a GET request to "+URL+". Is this ok?: 'y'+Enter= Continue, Enter= Abort Proxy Check\n"):
             #   return [ True, "Proxy Check OK: Proxy Check Aborted by User" ]
@@ -133,12 +164,20 @@ class Requester:
             if LogSettingBackup:
                 self.log_transactions(LogSettingBackup)
             RefusedAfter = self.RequestCountRefused
-            if RefusedBefore < RefusedAfter: # Proxy is refusing connections
-                return [ False, "ERROR: Proxy Check error: The proxy is not listening or is refusing connections" ]
+            if RefusedBefore < RefusedAfter:  # Proxy is refusing connections.
+                return [
+                    False,
+                    "ERROR: Proxy Check error: The proxy is not listening " \
+                    "or is refusing connections"]
             else:
-                return [ True, "Proxy Check OK: The proxy appears to be working" ]
-        return [ True, "Proxy Check OK: No proxy is setup or no HTTP requests will be made" ]
-    
+                return [
+                    True,
+                    "Proxy Check OK: The proxy appears to be working"]
+        return [
+            True,
+            "Proxy Check OK: No proxy is setup or no HTTP requests will " \
+            "be made"]
+
     def GetHeaders(self):
         return self.Headers
 
@@ -172,52 +211,65 @@ class Requester:
             POST = None
         if None != POST:
             if isinstance(POST, str) or isinstance(POST, unicode):
-                POST = self.StringToDict(POST) # Must be a dictionary prior to urlencode
+                # Must be a dictionary prior to urlencode.
+                POST = self.StringToDict(POST)
             POST = urllib.urlencode(POST)
         return POST
         #if Method == 'PUT':
         #   POST = "data="+POST
 
-
     def perform_request(self, request):
         return urllib2.urlopen(request)
 
-
     def set_succesful_transaction(self, RawRequest, Response):
         return self.Transaction.SetTransaction(True, RawRequest[0], Response)
-
 
     def log_transaction(self):
         self.Core.DB.Transaction.LogTransaction(self.Transaction)
 
     def Request(self, URL, Method = None, POST = None):
-        global RawRequest # kludge: necessary to get around urllib2 limitations: Need this to get the exact request that was sent
+        # kludge: necessary to get around urllib2 limitations: Need this to get
+        # the exact request that was sent.
+        global RawRequest
         URL = str(URL)
 
-        RawRequest = [] # Init Raw Request to blank list
+        RawRequest = []  # Init Raw Request to blank list.
         POST = self.DerivePOST(POST)
         Method = DeriveHTTPMethod(Method, POST)
-        URL = URL.strip() # Clean up URL
-        request = urllib2.Request(URL, POST, self.Headers) # GET request
+        URL = URL.strip()  # Clean up URL.
+        request = urllib2.Request(URL, POST, self.Headers)  # GET request.
         if None != Method:
-            request.get_method = lambda : Method # kludge: necessary to do anything other that GET or POST with urllib2
-        # MUST create a new Transaction object each time so that lists of transactions can be created and process at plugin-level
-        self.Transaction = transaction.HTTP_Transaction(self.Core.Timer) # Pass the timer object to avoid instantiating each time
-        self.Transaction.Start(URL, POST, Method, self.Core.DB.Target.IsInScopeURL(URL))
-        self.RequestCountTotal += 1 
+            # kludge: necessary to do anything other that GET or POST with
+            # urllib2
+            request.get_method = lambda : Method
+        # MUST create a new Transaction object each time so that lists of
+        # transactions can be created and process at plugin-level
+        # Pass the timer object to avoid instantiating each time.
+        self.Transaction = transaction.HTTP_Transaction(self.Core.Timer)
+        self.Transaction.Start(
+            URL,
+            POST,
+            Method,
+            self.Core.DB.Target.IsInScopeURL(URL))
+        self.RequestCountTotal += 1
         try:
             Response = self.perform_request(request)
             self.set_succesful_transaction(RawRequest, Response)
         except urllib2.HTTPError, Error: # page NOT found
-            self.Transaction.SetTransaction(False, RawRequest[0], Error) # Error is really a response for anything other than 200 OK in urllib2 :)
+            # Error is really a response for anything other than 200 OK in
+            # urllib2 :)
+            self.Transaction.SetTransaction(False, RawRequest[0], Error)
         except urllib2.URLError, Error: # Connection refused?
             ErrorMessage = self.ProcessHTTPErrorCode(Error, URL)
             self.Transaction.SetError(ErrorMessage)
         except IOError:
-            ErrorMessage = "ERROR: Requester Object -> Unknown HTTP Request error: "+URL+"\n"+str(sys.exc_info())
+            ErrorMessage = "ERROR: Requester Object -> Unknown HTTP " \
+                           "Request error: " + URL + "\n" + str(sys.exc_info())
             self.Transaction.SetError(ErrorMessage)
         if self.LogTransactions:
-            self.log_transaction() # Log transaction in DB for analysis later and return modified Transaction with ID
+            # Log transaction in DB for analysis later and return modified
+            # Transaction with ID.
+            self.log_transaction()
         return self.Transaction
 
     def ProcessHTTPErrorCode(self, Error, URL):
@@ -226,12 +278,14 @@ class Requester:
             Message = "ERROR: The connection was refused!: " +  str(Error)
             self.RequestCountRefused += 1 
         elif str(Error.reason).startswith("[Errno -2]"):
-            self.Core.Error.FrameworkAbort("ERROR: cannot resolve hostname!: " + str(Error))
+            self.Core.Error.FrameworkAbort(
+                "ERROR: cannot resolve hostname!: " + str(Error))
         else:
             Message = "ERROR: The connection was not refused, unknown error!"
         log = logging.getLogger('general')
         log.info(Message)
-        ErrorMessage = Message+" (Requester Object): "+URL+"\n"+str(sys.exc_info())
+        ErrorMessage = Message + " (Requester Object): " + URL + \
+                       "\n" + str(sys.exc_info())
         return ErrorMessage
 
     def GET(self, URL):
@@ -277,8 +331,14 @@ class Requester:
         self.Headers = dict.copy(self.HeadersBackup)
 
     def GetTransaction(self, UseCache, URL, Method = '', Data = ''):
-        Criteria = { 'url' : URL.strip(), 'method' : Method, 'data' : self.DerivePOSTToStr(Data) } # Must clean-up data to ensure match is found
-        if not UseCache or not self.Core.DB.Transaction.IsTransactionAlreadyAdded(Criteria): # Visit URL if not already visited
+        Criteria = {
+            'url': URL.strip(),
+            'method': Method,
+            # Must clean-up data to ensure match is found.
+            'data': self.DerivePOSTToStr(Data)}
+        # Visit URL if not already visited.
+        if (not UseCache or not
+                self.Core.DB.Transaction.IsTransactionAlreadyAdded(Criteria)):
             if Method in [ '', 'GET', 'POST', 'HEAD', 'TRACE', 'OPTIONS' ]:
                 return self.Request(URL, Method, Data)
                 #self.Request(URL, Method, Data)
@@ -288,19 +348,33 @@ class Requester:
             elif Method == 'PUT':
                 return self.PUT(URL, Data)
                 #self.PUT(URL, Data)
-        else: # Retrieve from DB = faster
+        else:  # Retrieve from DB = faster.
         #while True: #TODO: Make something better
-            return self.Core.DB.Transaction.GetFirst(Criteria) # Important since there is no transaction ID with transactions objects created by Requester
+            # Important since there is no transaction ID with transactions
+            # objects created by Requester.
+            return self.Core.DB.Transaction.GetFirst(Criteria)
 
-    def GetTransactions(self, UseCache, URLList, Method = '', Data = '', Unique = True):
+    def GetTransactions(self,
+                        UseCache,
+                        URLList,
+                        Method='',
+                        Data='',
+                        Unique=True):
         Transactions = []
         if Unique:
             URLList = set(URLList)
-        for URL in URLList: 
-            URL = URL.strip() # Clean up the URL first
-            if not URL: continue # skip blank lines 
-            if not self.Core.DB.URL.IsURL(URL): 
-                self.Core.Error.Add("Minor issue: "+str(URL)+" is not a valid URL and has been ignored, processing continues")
-                continue # Skip garbage URLs
-            Transactions.append(self.GetTransaction(UseCache, URL, Method, Data))
+        for URL in URLList:
+            URL = URL.strip()  # Clean up the URL first.
+            if not URL:
+                continue  # Skip blank lines.
+            if not self.Core.DB.URL.IsURL(URL):
+                self.Core.Error.Add(
+                    "Minor issue: " + str(URL) + " is not a valid URL and "
+                    "has been ignored, processing continues")
+                continue  # Skip garbage URLs.
+            Transactions.append(self.GetTransaction(
+                UseCache,
+                URL,
+                Method,
+                Data))
         return Transactions
