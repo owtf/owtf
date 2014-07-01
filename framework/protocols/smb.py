217dbf5f64fde019b300e0a485c245240e5d8a3d
Tao Sauvage <sauvage.tao@gmail.com>
2014-07-01 16:36:35 +0200

    [smb] PEP8 misc fixes.

--- framework/protocols/smb.py
+++ framework/protocols/smb.py
@@ -63,42 +63,51 @@ class SMB(pexpect_shell.PExpectShell):
             return True
         cprint("Initialising shell..")
         self.Open(options, plugin_info)
-        cprint("Ensuring Mount Point " + options['SMB_MOUNT_POINT'] + " exists..")
+        cprint(
+            "Ensuring Mount Point " + options['SMB_MOUNT_POINT'] + " exists..")
         self.check_mount_point_existence(options)
-        mount_cmd = "smbmount //" + options['SMB_HOST'] + "/" + options['SMB_SHARE'] + " " + options['SMB_MOUNT_POINT']
-        if options['SMB_USER']: # Pass user if specified
+        mount_cmd = "smbmount //" + options['SMB_HOST'] + "/" + \
+                    options['SMB_SHARE'] + " " + options['SMB_MOUNT_POINT']
+        if options['SMB_USER']:  # Pass user if specified.
             mount_cmd += " -o user=" + options['SMB_USER']
         cprint("Mounting share..")
         self.Run(mount_cmd)
         self.Expect("Password:")
-        if options['SMB_PASS']: # Pass password if specified
+        if options['SMB_PASS']:  # Pass password if specified.
             self.Run(options['SMB_PASS'])
         else:
-            self.Run("")  # Send blank line
+            self.Run("")  # Send blank line.
         self.Expect("#")
         self.SetMounted(True)
 
     def Transfer(self):
         operation = False
         if self.Options['SMB_DOWNLOAD']:
-            self.Download(self.Options['SMB_MOUNT_POINT'] + "/" + self.Options['SMB_DOWNLOAD'], ".")
+            self.Download(
+                self.Options['SMB_MOUNT_POINT'] + "/" + self.Options['SMB_DOWNLOAD'],
+                ".")
             operation = True
         if self.Options['SMB_UPLOAD']:
-            self.Upload(self.Options['SMB_UPLOAD'], self.Options['SMB_MOUNT_POINT'])
+            self.Upload(
+                self.Options['SMB_UPLOAD'],
+                self.Options['SMB_MOUNT_POINT'])
             operation = True
         if not operation:
             cprint("Nothing to do: no SMB_DOWNLOAD or SMB_UPLOAD specified..")
 
     def UnMount(self, plugin_info):
         if self.IsMounted():
-            self.Core.Shell.shell_exec_monitor("umount " + self.Options['SMB_MOUNT_POINT'])
+            self.Core.Shell.shell_exec_monitor(
+                "umount " + self.Options['SMB_MOUNT_POINT'])
             self.SetMounted(False)
             self.Close(plugin_info)
 
     def Upload(self, file_path, mount_point):
         cprint("Copying '" + file_path + "' to '" + mount_point + "'")
-        self.Core.Shell.shell_exec_monitor("cp -r " + file_path + " " + mount_point)
+        self.Core.Shell.shell_exec_monitor(
+            "cp -r " + file_path + " " + mount_point)
 
     def Download(self, remote_file_path, target_dir):
         cprint("Copying '" + remote_file_path + "' to '" + target_dir + "'")
-        self.Core.Shell.shell_exec_monitor("cp -r " + remote_file_path + " " + target_dir)
+        self.Core.Shell.shell_exec_monitor(
+            "cp -r " + remote_file_path + " " + target_dir)
