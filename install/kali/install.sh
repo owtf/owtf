de1ea8e17e619c8c442ea07421fc053564982dde
delta24 <viyat001@gmail.com>
2014-08-04 15:28:24 +0530

    [install] Shifted the python-owasp-zap-v2 pkg from Kali script to owtf.pip

--- install/kali/install.sh
+++ install/kali/install.sh
@@ -2,10 +2,10 @@
 # owtf is an OWASP+PTES-focused try to unite great tools and facilitate pen testing
 # Copyright (c) 2011, Abraham Aranguren <name.surname@gmail.com> Twitter: @7a_ http://7-a.org
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright 
+# * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright
 # notice, this list of conditions and the following disclaimer in the
@@ -13,7 +13,7 @@
 # * Neither the name of the copyright owner nor the
 # names of its contributors may be used to endorse or promote products
 # derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -55,9 +55,6 @@ sudo -E apt-get install lbd gnutls-bin arachni
 echo "[*] Installing ProxyChains"
 sudo -E apt-get install proxychains
 
-echo "[*] Installing ZAP API"
-pip install python-owasp-zap-v2
-
 ########## Patch scripts
 "$RootDir/install/kali/kali_patch_w3af.sh"
 "$RootDir/install/kali/kali_patch_nikto.sh"
@@ -65,7 +62,7 @@ pip install python-owasp-zap-v2
 ###### Dictionaries missing in Kali
 cd $RootDir/dictionaries/restricted
 IsInstalled "dirbuster"
-if [ $? -eq 0 ]; then # Not installed    
+if [ $? -eq 0 ]; then # Not installed
     # Copying dirbuster dicts
     echo "\n[*] Copying Dirbuster dictionaries"
     mkdir -p dirbuster
