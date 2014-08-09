ba4a070f49b580673fd2dd955717996c8a18bb61
Lucif3r <anirudhanand722@gmail.com>
2014-08-09 21:15:16 +0530

    [db]Scripts for creating Plugin Mapping and vuln explanations (databases) is added to framework/scripts

--- framework/scripts/retreiver.py
+++ framework/scripts/retreiver.py
@@ -1,11 +1,12 @@
+"""For retreving Database path from framework/config/framework_config.cfg"""
 import codecs
 import os
 
 def retreiver():
 	abs_path = os.path.dirname(os.path.abspath(__file__))
 	home=os.getenv("HOME")
-	check="DATABASE_LOCATION:"
-	path=os.path.join(abs_path,'../../profiles/general/default.cfg')
+	check="MAPPINGS_DATABASE_PATH:"
+	path=os.path.join(abs_path,'../../framework/config/framework_config.cfg')
 	filer=codecs.open(path)
 	for index,ln in enumerate(filer):
 		if check in ln:
--- framework/scripts/vuln_database.py
+++ framework/scripts/vuln_database.py
@@ -4,7 +4,9 @@ import urllib2
 import sys
 import glob
 import os
-from sqlalchemy import *
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import codecs
@@ -13,6 +15,7 @@ from retreiver import retreiver
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 file_path=os.path.join(abs_path,"../../framework/scripts/commit-hash")
+#getting the old commit-hash for comparison
 old_hash=""
 ink = codecs.open(file_path,mode='r')
 hash=ink.readlines()
@@ -21,6 +24,7 @@ for ln in hash:
 
 new_hash=""
 p = subprocess.Popen("git ls-remote https://github.com/owtf/boilerplate-templates.git | grep HEAD | awk '{print $1}' ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#Getting the latest commit hash from github
 for line in p.stdout.readlines():
     new_hash+=line
 retval = p.wait()
@@ -29,10 +33,11 @@ if new_hash!=old_hash:
 	os.remove(file_path)
 	file=codecs.open(file_path,'w')
 	file.write(new_hash)
+	#Updating the file with latest commit hash
 	file.close
 
-	GITHUB_REPOS_API_BASE_URL = 'https://api.github.com/repos/'
 
+	GITHUB_REPOS_API_BASE_URL = 'https://api.github.com/repos/'
 	def write_file(item, dir_name):
 	    name = item['name']
 	    res = urllib2.urlopen(item['url']).read()
@@ -57,6 +62,7 @@ if new_hash!=old_hash:
 	if __name__ == '__main__':
 	    args = dict(enumerate(sys.argv))
 	    path = 'owtf/boilerplate-templates/_posts'
+	    #Cloning a particular sub directory from git (for DB updation)
 	    path = path.split('/')
 	    new_dir_name = path[-1]
 	    if os.path.exists(new_dir_name):
@@ -70,13 +76,14 @@ if new_hash!=old_hash:
 
 
 	ExpBase = declarative_base()
-	class VulnExp(ExpBase):
-		__tablename__ = 'VulnExp'
+	class Vulnexp(ExpBase):
+		__tablename__ = 'Vulnexp'
 		Title = Column(String, primary_key=True)
 		Desc = Column(String)
 		Category=Column(String)
 
 	path=retreiver()
+	#getting the path for db creation
 	path='sqlite:///'+path+'vulnexp.db'
 	engine = create_engine(path)
 	session = sessionmaker()
@@ -104,11 +111,11 @@ if new_hash!=old_hash:
 		title2=title.split(":")
 		vuln = {'Title': title2[1], 'Desc':texto }
 		vuln_desc = json.dumps(texto)
-		obj = VulnExp(Title = vuln['Title'],Desc=vuln_desc,Category=lock)
+		obj = Vulnexp(Title = vuln['Title'],Desc=vuln_desc,Category=lock)
 		s.add(obj)
-		s.commit()
 		line=""
 		title=""
 		texto=""
+	s.commit()
 else:
 	print("No update necessary")
