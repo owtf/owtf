a34325566a84630c0de7218dc70bc132cb970285
owtf <owtf@owt>
2015-07-04 01:44:16 +0100

    OWTF 1.0.1 Lion_Heart: Cleaning up files and adding debian scripts

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -31,9 +31,10 @@
 
 get_config_value(){
 
+    current_user=$(logname)
     parameter=$1
-    file=$2
-
+    file="/home/$current_user/.owtf/db.cfg"
+     
     echo "$(grep -i $parameter $file | sed  "s|$parameter: ||g;s|~|$HOME|g")"
 }
 
