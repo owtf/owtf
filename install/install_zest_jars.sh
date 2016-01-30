fc55ca1b0a91da1130f604abab8fcb8004c772b6
delta24 <viyat001@gmail.com>
2016-01-30 10:52:33 +0530

    Fixed zest install errors

--- install/install_zest_jars.sh
+++ install/install_zest_jars.sh
@@ -3,24 +3,34 @@
 FILE_PATH=$(readlink -f "$0")
 INSTALL_DIR=$(dirname "$FILE_PATH")
 RootDir=${RootDir:-$(dirname "$INSTALL_DIR")}
+local_hash_file="$RootDir/zest/release.hash"
 
-local_hash="$(cat $RootDir/zest/release.hash)"
-upstream_hash="$(wget -O- -q https://raw.githubusercontent.com/owtf/owtf-zest-jars/master/release.hash)"
-install_command="wget https://api.github.com/repos/owtf/owtf-zest-jars/tarball -O zest-jars.tar.gz; tar zxf zest-jars.tar.gz; cd */.; cp -r * $RootDir/zest"
-
-
-# check if local hash present
-if [ ! $local_hash ]; then
-  echo "Local hash not present"
-  $install_command
+if [ -f $local_hash_file ]; then
+  local_hash="$(cat $local_hash_file)"
+else
+  local_hash=""
 fi
 
-echo "Local hash present, comparing with upstream.."
+upstream_hash="$(wget -O- -q https://raw.githubusercontent.com/owtf/owtf-zest-jars/master/release.hash)"
 
-if ! [ "$local_hash" = "$upstream_hash" ]; then
-  echo "Hashes do not match, updating jars.."
-  $install_command
+install() {
+  wget https://api.github.com/repos/owtf/owtf-zest-jars/tarball -O zest-jars.tar.gz
+  tar zxf zest-jars.tar.gz
+  cd */.
+  cp -r * $RootDir/zest
+}
+
+# check if local hash present
+if [ -z $local_hash ]; then
+  echo "Local hash not present"
+  install
 else
-  echo "Hashes match. Continuing.."
+  echo "Local hash present, comparing with upstream.."
+  if ! [ "$local_hash" = "$upstream_hash" ]; then
+    echo "Hashes do not match, updating jars.."
+    install
+  else
+    echo "Hashes match. Continuing.."
+  fi
 fi
 
