5014ee686f8766613ef663254e235f977ba6ca7f
karandesai-96 <karandesai_96@live.com>
2016-03-03 13:52:04 +0530

    Adds Colorizer class in python install script.
    - It has four class methods which print in different colors
      according to type of message:
    	- normal (white)
    	- info (cyan)
    	- warning (yellow)
    	- danger (red)
    
    - All the output printed by script in made bold to locate
      it easily in the huge output log, as bash commands also
      generate their own log.
    
    - All the print statements in script are modified according
      to general inspection whether the statement is a normal
      statement indicating process, an informative statement, a
      warning or a danger.

--- install/install.py
+++ install/install.py
@@ -38,8 +38,8 @@ class Installer(object):
             return False
 
     def run_command(self, command):
-        print("[*] Running following command")
-        print("%s"%(command))
+        Colorizer.normal("[*] Running following command")
+        Colorizer.info("%s"%(command))
         if not wget_wrapper(command):
             return
         os.system(command)
@@ -62,7 +62,8 @@ class Installer(object):
         if sudo == 0:
             return
         else:
-            print("[WARNING] Your user does not have sudo priveleges. Some OWTF components require sudo permissions to install")
+            Colorizer.warning("[!] Your user does not have sudo privileges. "
+                              "Some OWTF components require sudo permissions to install")
             # exit cleanly
             sys.exit()
 
@@ -82,7 +83,7 @@ class Installer(object):
         cp = ConfigParser.ConfigParser({"RootDir":self.RootDir, "Pid":self.pid})
         cp.read(config_file)
         for section in cp.sections():
-            print("[*] Installing %s"%(section))
+            Colorizer.info("[*] Installing %s"%(section))
             self.install_in_directory(os.path.expanduser(cp.get(section, "directory")), cp.get(section, "command"))
 
     def install(self, cmd_arguments):
@@ -106,7 +107,8 @@ class Installer(object):
         # Loop until proper input is received
         while True:
             if distro_num != 0:
-                print("[*] %s has been automatically detected... Continuing in auto-mode"%(distro))
+                Colorizer.info("[*] %s has been automatically detected... "%distro)
+                Colorizer.normal("Continuing in auto-mode")
                 break
 
             if args.no_user_input:
@@ -115,15 +117,16 @@ class Installer(object):
 
             print("")
             for i in range(0, len(cp.sections())):
-                print("(%d) %s"%(i+1, cp.sections()[i]))
-            print("(0) %s (%s)"%("My distro is not listed :(", distro))
-            distro_num = raw_input("Select a number based on your distribution : ")
+                Colorizer.warning("(%d) %s"%(i+1, cp.sections()[i]))
+            Colorizer.warning("(0) %s (%s)"%("My distro is not listed :(", distro))
+
+            distro_num = raw_input(Colorizer.normal("Select a number based on your distribution : "))
             try: # Cheking if valid input is received
                 distro_num = int(distro_num)
                 break
             except ValueError:
                 print('')
-                print("Invalid Number specified")
+                Colorizer.warning("Invalid Number specified")
                 continue
 
         # First all distro independent stuff is installed
@@ -132,14 +135,14 @@ class Installer(object):
         if distro_num != 0:
             self.run_command(cp.get(cp.sections()[int(distro_num)-1], "install"))
         else:
-            print("Skipping distro related installation :(")
+            Colorizer.normal("Skipping distro related installation :(")
 
 
 
         if args.core_only:
             return
 
-        print("Upgrading pip to the latest version ...")
+        Colorizer.normal("Upgrading pip to the latest version ...")
         # Upgrade pip before install required libraries
         self.run_command("sudo pip2 install --upgrade pip")
         # mitigate cffi errors by upgrading it first
@@ -157,19 +160,22 @@ class Installer(object):
 
                 if fixsetuptools == 'y':
                     # backup the original symlink
-                    print("Backing up the original symlink...")
+                    Colorizer.normal("Backing up the original symlink...")
                     ts = time.time()
                     human_timestamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H:%M:%S')
 
                     # backup the original symlink
                     self.run_command("mv /usr/lib/python2.7/dist-packages/setuptools.egg-info /usr/lib/python2.7/dist-packages/setuptools.egg-info" + "-BACKUP-" + human_timestamp)
 
-                    print("The original symlink exists at /usr/lib/python2.7/dist-packages/setuptools.egg-info-BACKUP-" + human_timestamp)
+                    Colorizer.info("The original symlink exists at "
+                          "/usr/lib/python2.7/dist-packages/setuptools.egg-info-BACKUP-"
+                          + human_timestamp)
 
                     # Finally owtf python libraries installed using pip
                     self.install_using_pip(self.owtf_pip)
             else:
-                print("Moving on with the installation but you were warned: there may be some errors!")
+                Colorizer.warning("Moving on with the installation but you "
+                                  "were warned: there may be some errors!")
 
         # Finally owtf python libraries installed using pip
         self.install_using_pip(self.owtf_pip)
@@ -179,13 +185,51 @@ class Installer(object):
         # db_run script
         self.run_command("sudo sh %s" % (os.path.join(self.scripts_path, "db_run.sh")))
 
+
+class Colorizer:
+    BOLD = '\033[1m'
+    RED = BOLD + '\033[91m'
+    GREEN = BOLD + '\033[92m'
+    YELLOW = BOLD + '\033[93m'
+    BLUE = BOLD + '\033[34m'
+    PURPLE = BOLD + '\033[95m'
+    CYAN = BOLD + '\033[36m'
+    WHITE = BOLD + '\033[37m'
+    END = '\033[0m\033[0m'
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def normal(cls, string):
+        print(cls.WHITE + string + cls.END)
+
+    @classmethod
+    def info(cls, string):
+        print(cls.CYAN + string + cls.END)
+
+    @classmethod
+    def warning(cls, string):
+        print(cls.YELLOW + string + cls.END)
+
+    @classmethod
+    def success(cls, string):
+        print(cls.GREEN + string + cls.END)
+
+    @classmethod
+    def danger(cls, string):
+        print(cls.RED + string + cls.END)
+
+
 if __name__ == "__main__":
-    print("[*] Great that you are installing OWTF :D")
-    print("[!] There will be lot of output, please be patient")
+    Colorizer.normal("[*] Great that you are installing OWTF :D")
+    Colorizer.warning("[!] There will be lot of output, please be patient")
+
     RootDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     installer = Installer(RootDir)
-    print("[DEBUG] Last commit hash: %s" % installer.version(RootDir))
+    Colorizer.info("[DEBUG] Last commit hash: %s" % installer.version(RootDir))
     installer.check_sudo()
     installer.install(sys.argv[1:])
-    print("[*] Finished!")
-    print("[*] Start OWTF by running \033[0;34m./owtf.py\033[0m in the parent directory")
+
+    Colorizer.success("[*] Finished!")
+    Colorizer.info("[*] Start OWTF by running './owtf.py' in parent directory")
