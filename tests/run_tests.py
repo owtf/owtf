532be220de1430784367b6ba41ab38ba7d43d616
karandesai-96 <karandesai_96@live.com>
2016-03-13 18:03:29 +0530

    Code cleanup for tests/run_tests.py
    - Removes redundant import statements. os_path and
      sys_path are not as much in practice as os.path
      and sys.path themselves.
    
    - include_owtf_path_in_pythonpath() methods was made
      and just once used - in the very next line. This
      redundant code was cleaned up.
    
    - sys.argv[1:len(sys.argv)] is redundant, just using
      sys.argv[1:] is sufficient.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python
-'''
+"""
 This is the command-line tool for running tests.
-'''
-
+"""
+import os
+import sys
 from testing_framework.runner.parser import ArgumentParser
 from testing_framework.runner.arg_builder import NoseArgumentBuilder
 from testing_framework.runner.runner import Runner
-from sys import argv
-from os import path as os_path
-from sys import path as sys_path
 
 
-def include_owtf_path_in_pythonpath():
-    framework_path = os_path.abspath('..')
-    print "[+] Setting up environment..."
-    sys_path.append(framework_path)
-    print framework_path + " appended to the PYTHON_PATH"
+framework_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+print "[+] Setting up environment..."
+sys.path.append(framework_path)
+print framework_path + " appended to the PYTHON_PATH"
 
-
-include_owtf_path_in_pythonpath()
 parser = ArgumentParser()
-if (len(argv) == 1):
+if len(sys.argv) == 1:
     parser.print_usage()
 else:
-    args = parser.parse_arguments(argv[1:len(argv)])
+    args = parser.parse_arguments(sys.argv[1:])
     nose_arguments = NoseArgumentBuilder(args).build()
     runner = Runner(nose_arguments)
     print "[+] Running tests..."
-    if (args.coverage is not None) and (args.all == True):
+    if args.coverage is not None and args.all:
         # Coverage reports only have sense with all test
         runner.run_nose_with_coverage()
     else:
