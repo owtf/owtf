f5a5119c6f961a4110b19587201b39ac18536d73
DoomTaper <ayush.singh.ece13@itbhu.ac.in>
2016-04-01 05:23:50 -0400

    Dirbuster plugin working fixes #642

--- plugins/web/active/Old_Backup_and_Unreferenced_Files@OWTF-CM-006.py
+++ plugins/web/active/Old_Backup_and_Unreferenced_Files@OWTF-CM-006.py
@@ -12,6 +12,8 @@ def run(PluginInfo):
 	# DirBuster allows much more control when interactive
 	# DirBuster can also be run non-interactively for scripting
 	DirBusterInteraction = { 'true' : 'DirBusterInteractive', 'false' : 'DirBusterNotInteractive' }
-	return ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', ServiceLocator.get_component("resource").GetResourceList([ DirBusterInteraction[ServiceLocator.get_component("db_config").Get('INTERACTIVE')], 'DirBuster_Extract_URLs' ]), PluginInfo, [])
+	Content = ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', ServiceLocator.get_component("resource").GetResources(DirBusterInteraction[ServiceLocator.get_component("db_config").Get('INTERACTIVE')]), PluginInfo, [])
+	Content += ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', ServiceLocator.get_component("resource").GetResources('DirBuster_Extract_URLs'), PluginInfo, [])
+	return Content
 	#return ServiceLocator.get_component("plugin_helper").DrawCommandDump('Test Command', 'Output', ServiceLocator.get_component("config").GetResources(DirBusterInteraction[ServiceLocator.get_component("config").Get('Interactive')]), PluginInfo, Content)
 
