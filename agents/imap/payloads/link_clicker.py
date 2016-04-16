0404911bf72b528ff0e840f76331db388dcb7230
Parth Oberoi <htrapdev@gmail.com>
2016-04-16 18:14:51 +0530

    Fix: Pep8 fixes for /agents
    Signed-off-by: Parth Oberoi <htrapdev@gmail.com>

--- agents/imap/payloads/link_clicker.py
+++ agents/imap/payloads/link_clicker.py
@@ -3,17 +3,20 @@
 Description:
 OWTF imap agent daemon plugin, to emulate user clicks via email
 '''
-import re, subprocess
+
+import re
+import subprocess
+
 URL_REGEX = 'http[:0-9a-zA-Z\.\/]+'
-#TODO: Play with below and see if it is better or not
+# TODO: Play with below and see if it is better or not
 # http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
-#url_regex = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
+
 
 def Run(Params):
-	LogStr = " >> " + Params['Log'] + " 2>> " + Params['ErrorLog']
-	for URL in re.findall(URL_REGEX, str(Params['Message'])):
-		print "Found URL=" + URL
-		VisitURLCmd = "curl " + URL
-		subprocess.Popen(VisitURLCmd, shell=True) # Visit URL
-		# Log visit:
-		subprocess.Popen('echo "$(date)" - ' + VisitURLCmd + LogStr, shell=True)
+    LogStr = " >> %s 2>> %s" % (Params['Log'], Params['ErrorLog'])
+    for URL in re.findall(URL_REGEX, str(Params['Message'])):
+        print "Found URL=%s" % URL
+        VisitURLCmd = "curl %s" % URL
+        subprocess.Popen(VisitURLCmd, shell=True)  # Visit URL
+        # Log visit:
+        subprocess.Popen('echo "$(date)" - %s%s' % (VisitURLCmd, LogStr), shell=True)
