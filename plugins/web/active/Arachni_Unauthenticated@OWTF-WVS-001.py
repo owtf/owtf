2a05b73b5912dc5570bd77ea604b04e4daf3c8f1
delta24 <viyat001@gmail.com>
2016-05-22 09:11:36 +0530

    [PEP8] Cleanup web/active plugins

--- plugins/web/active/Arachni_Unauthenticated@OWTF-WVS-001.py
+++ plugins/web/active/Arachni_Unauthenticated@OWTF-WVS-001.py
@@ -1,12 +1,15 @@
-from framework.dependency_management.dependency_resolver import ServiceLocator
 """ 
 ACTIVE Plugin for Generic Unauthenticated Web App Fuzzing via Arachni
 This will perform a "low-hanging-fruit" pass on the web app for easy to find (tool-findable) vulns
 
 """
 
+from framework.dependency_management.dependency_resolver import ServiceLocator
+
+
 DESCRIPTION = "Active Vulnerability Scanning without credentials via Arachni"
 
+
 def run(PluginInfo):
-	#ServiceLocator.get_component("config").Show()
-	return ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', ServiceLocator.get_component("resource").GetResources('Arachni_Unauth'), PluginInfo, [])
+    return ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', 
+        ServiceLocator.get_component("resource").GetResources('Arachni_Unauth'), PluginInfo, [])
--- plugins/web/active/Wapiti_Unauthenticated@OWTF-WVS-003.py
+++ plugins/web/active/Wapiti_Unauthenticated@OWTF-WVS-003.py
@@ -1,11 +1,14 @@
-from framework.dependency_management.dependency_resolver import ServiceLocator
 """ 
 ACTIVE Plugin for Generic Unauthenticated Web App Fuzzing via Wapiti
 This will perform a "low-hanging-fruit" pass on the web app for easy to find (tool-findable) vulns
 """
 
+from framework.dependency_management.dependency_resolver import ServiceLocator
+
+
 DESCRIPTION = "Active Vulnerability Scanning without credentials via Wapiti"
 
+
 def run(PluginInfo):
-	#ServiceLocator.get_component("config").Show()
-	return ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', ServiceLocator.get_component("resource").GetResources('Wapiti_Unauth'), PluginInfo, [])
+    return ServiceLocator.get_component("plugin_helper").CommandDump('Test Command', 'Output', 
+        ServiceLocator.get_component("resource").GetResources('Wapiti_Unauth'), PluginInfo, [])
