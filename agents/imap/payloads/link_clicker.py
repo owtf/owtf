b36bc85039d436aa4eeb3214049aa1145c50ed23
delta24 <viyat001@gmail.com>
2016-05-28 19:26:56 +0530

    [PEP8] Cleanup agents/

--- agents/imap/payloads/link_clicker.py
+++ agents/imap/payloads/link_clicker.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 '''
-Description:
-OWTF imap agent daemon plugin, to emulate user clicks via email
+Description: OWTF imap agent daemon plugin, to emulate user clicks via email
 '''
-import re, subprocess
-URL_REGEX = 'http[:0-9a-zA-Z\.\/]+'
-#TODO: Play with below and see if it is better or not
-# http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
-#url_regex = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
 
-def Run(Params):
-	LogStr = " >> " + Params['Log'] + " 2>> " + Params['ErrorLog']
-	for URL in re.findall(URL_REGEX, str(Params['Message'])):
-		print "Found URL=" + URL
-		VisitURLCmd = "curl " + URL
-		subprocess.Popen(VisitURLCmd, shell=True) # Visit URL
-		# Log visit:
-		subprocess.Popen('echo "$(date)" - ' + VisitURLCmd + LogStr, shell=True)
+import re
+import subprocess
+
+# TODO: Play with below and see if it is better or not
+# http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
+# url_regex = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
+URL_REGEX = 'http[:0-9a-zA-Z\.\/]+'
+
+
+def run(params):
+    log_str = " >> %s 2>> %s" % (params['log'], params['error_log'])
+    for url in re.findall(URL_REGEX, str(params['message'])):
+        print "Found URL=%s" % url
+        visit_url_cmd = "curl %s" % url
+        subprocess.Popen(visit_url_cmd, shell=True) # Visit URL
+        # Log visit
+        subprocess.Popen('echo "$(date)" - %s' % (VisitURLCmd+LogStr), shell=True)
