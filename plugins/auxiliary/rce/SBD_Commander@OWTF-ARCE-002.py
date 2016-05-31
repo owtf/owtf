599b9ec9b3bde6adda50640ba22235b80f904396
delta24 <viyat001@gmail.com>
2016-05-31 15:46:42 +0530

    [PEP8] Cleanup plugins/auxiliary

--- plugins/auxiliary/rce/SBD_Commander@OWTF-ARCE-002.py
+++ plugins/auxiliary/rce/SBD_Commander@OWTF-ARCE-002.py
@@ -1,33 +1,37 @@
 from framework.dependency_management.dependency_resolver import ServiceLocator
 
+
 DESCRIPTION = "Runs commands on an agent server via SBD -i.e. for IDS testing-"
 
 
 def run(PluginInfo):
-    # ServiceLocator.get_component("config").Show()
     Core = ServiceLocator.get_component("core")
     Content = DESCRIPTION + " Results:<br />"
     plugin_params = ServiceLocator.get_component("plugin_params")
     config = ServiceLocator.get_component("config")
-    for Args in plugin_params.GetArgs({
-                                                                          'Description': DESCRIPTION,
-                                                                          'Mandatory': {
-                                                                          'RHOST': config.Get('RHOST_DESCRIP'),
-                                                                          'SBD_PORT': config.Get('SBD_PORT_DESCRIP'),
-                                                                          'SBD_PASSWORD': config.Get('SBD_PASSWORD_DESCRIP'),
-                                                                          'COMMAND_FILE': config.Get('COMMAND_FILE_DESCRIP')
-                                                                          },
-                                                                          'Optional': {
-                                                                          'REPEAT_DELIM': config.Get('REPEAT_DELIM_DESCRIP')
-                                                                          }}, PluginInfo):
+
+    args = {
+        'Description': DESCRIPTION,
+        'Mandatory': {
+            'RHOST': config.Get('RHOST_DESCRIP'),
+            'SBD_PORT': config.Get('SBD_PORT_DESCRIP'),
+            'SBD_PASSWORD': config.Get('SBD_PASSWORD_DESCRIP'),
+            'COMMAND_FILE': config.Get('COMMAND_FILE_DESCRIP')
+        },
+        'Optional': {'REPEAT_DELIM': config.Get('REPEAT_DELIM_DESCRIP')}
+    }
+
+    for Args in plugin_params.GetArgs(args, PluginInfo):
         plugin_params.SetConfig(Args)  # Sets the auxiliary plugin arguments as config
 
-        #print "Args="+str(Args)
         Core.RemoteShell.Open({
-                              'ConnectVia': config.GetResources('RCE_SBD_Connection')
-                              , 'InitialCommands': None
-                              }, PluginInfo)
+            'ConnectVia': config.GetResources('RCE_SBD_Connection'),
+            'InitialCommands': None
+        }, PluginInfo)
         Core.RemoteShell.RunCommandList(Core.GetFileAsList(Args['COMMAND_FILE']))
         Core.RemoteShell.Close(PluginInfo)
-    #Content += ServiceLocator.get_component("plugin_helper").DrawCommandDump('Test Command', 'Output', ServiceLocator.get_component("config").GetResources('LaunchExploit_'+Args['CATEGORY']+"_"+Args['SUBCATEGORY']), PluginInfo, "") # No previous output
+    resource = ServiceLocator.get_component("config").GetResources('LaunchExploit_' + Args['CATEGORY'] + "_" +
+                                                                   Args['SUBCATEGORY'])
+    Content += ServiceLocator.get_component("plugin_helper").DrawCommandDump('Test Command', 'Output', resource,
+                                                                             PluginInfo, "")  # No previous output
     return Content
