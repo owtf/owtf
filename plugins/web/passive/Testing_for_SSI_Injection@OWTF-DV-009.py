9f0382b9a1543adc63e1f0d0a89db1b94f211c2b
delta24 <viyat001@gmail.com>
2016-05-31 14:43:18 +0530

    [PEP8] Cleanup plugins/web/passive

--- plugins/web/passive/Testing_for_SSI_Injection@OWTF-DV-009.py
+++ plugins/web/passive/Testing_for_SSI_Injection@OWTF-DV-009.py
@@ -1,11 +1,14 @@
-from framework.dependency_management.dependency_resolver import ServiceLocator
 """
 PASSIVE Plugin for Testing_for_SSI_Injection@OWASP-DV-009
 """
 
+from framework.dependency_management.dependency_resolver import ServiceLocator
+
+
 DESCRIPTION = "Searching for pages that are susceptible to SSI-Injection"
 
+
 def run(PluginInfo):
-	#ServiceLocator.get_component("config").Show()
-	Content = ServiceLocator.get_component("plugin_helper").ResourceLinkList('Online Resources', ServiceLocator.get_component("resource").GetResources('PassiveSSIDiscoveryLnk'))
-	return Content
+    resource = ServiceLocator.get_component("resource").GetResources('PassiveSSIDiscoveryLnk')
+    Content = ServiceLocator.get_component("plugin_helper").ResourceLinkList('Online Resources', resource)
+    return Content
--- plugins/web/passive/Web_Application_Fingerprint@OWTF-IG-004.py
+++ plugins/web/passive/Web_Application_Fingerprint@OWTF-IG-004.py
@@ -1,25 +1,25 @@
-from framework.dependency_management.dependency_resolver import ServiceLocator
-
 """
 PASSIVE Plugin for Testing for Web Application Fingerprint (OWASP-IG-004)
 """
+
+from framework.dependency_management.dependency_resolver import ServiceLocator
+
 
 DESCRIPTION = "Third party resources and fingerprinting suggestions"
 
 
 def run(PluginInfo):
-    # ServiceLocator.get_component("config").Show()
-    #Vuln search box to be built in core and reused in different plugins:
+    mapping = [
+        ['All', 'CMS_FingerPrint_All'],
+        ['WordPress', 'CMS_FingerPrint_WordPress'],
+        ['Joomla', 'CMS_FingerPrint_Joomla'],
+        ['Drupal', 'CMS_FingerPrint_Drupal'],
+        ['Mambo', 'CMS_FingerPrint_Mambo']
+    ]
+    # Vuln search box to be built in core and reused in different plugins:
     plugin_helper = ServiceLocator.get_component("plugin_helper")
     Content = plugin_helper.VulnerabilitySearchBox('')
-    Content += plugin_helper.ResourceLinkList('Online Resources', ServiceLocator.get_component("resource").GetResources('PassiveFingerPrint'))
-    Content += plugin_helper.SuggestedCommandBox(PluginInfo,
-                                                                                 [['All', 'CMS_FingerPrint_All'],
-                                                                                  ['WordPress',
-                                                                                   'CMS_FingerPrint_WordPress'],
-                                                                                  ['Joomla', 'CMS_FingerPrint_Joomla'],
-                                                                                  ['Drupal', 'CMS_FingerPrint_Drupal'],
-                                                                                  ['Mambo', 'CMS_FingerPrint_Mambo']],
-                                                                                 'CMS Fingerprint - Potentially useful commands')
+    resource = ServiceLocator.get_component("resource").GetResources('PassiveFingerPrint')
+    Content += plugin_helper.ResourceLinkList('Online Resources', resource)
+    Content += plugin_helper.SuggestedCommandBox(PluginInfo, mapping, 'CMS Fingerprint - Potentially useful commands')
     return Content
-
