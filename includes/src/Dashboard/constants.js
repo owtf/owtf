440d64aa88f0240f281c3179fda996ca16b04c0c
saganshul <sganshuliiith@gmail.com>
2016-06-24 03:39:03 +0530

    [webui] Constants created for WorkerPanel

--- includes/src/Dashboard/constants.js
+++ includes/src/Dashboard/constants.js
@@ -1,2 +1,4 @@
 export const SEVERITY_CHART_URL = '/api/targets/severitychart/'
 export const SEVERITY_PANEL_URL = '/api/dashboard/severitypanel/'
+export const WORKER_PANEL_URL = '/api/plugins/progress/'
+export const WORKER_DETAIL_URL = '/api/workers/'
