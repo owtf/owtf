92c5a033b5d6dc46db24f84c3db4fa026a0dca46
saganshul <sganshuliiith@gmail.com>
2016-06-24 04:18:16 +0530

    Corresponding changes made in Chart.js to maintain mobile responsiveness

--- includes/src/Dashboard/Chart.js
+++ includes/src/Dashboard/Chart.js
@@ -7,14 +7,10 @@ class Severity extends React.Component {
         return (
             <div className="row">
                 <li>
-                    <div className="col-xs-4 col-md-2">
-                        <div className="legend-color-box" style={{
-                            "backgroundColor": this.props.data.color
-                        }}></div>
-                    </div>
-                    <div className="col-xs-8 col-md-10">
-                        <p className="severity-label">{this.props.data.label}</p>
-                    </div>
+                    <div className="legend-color-box" style={{
+                        "backgroundColor": this.props.data.color
+                    }}></div>
+                    <p className="severity-label">{this.props.data.label}</p>
                 </li>
             </div>
         );
@@ -26,7 +22,6 @@ class ChartLegend extends React.Component {
     render() {
         return (
             <ul className="severity-legend">
-
                 {this.props.datasets.map(function(severity) {
                     return <Severity key={severity.id} data={severity}/>;
                 })}
@@ -59,12 +54,20 @@ class Chart extends React.Component {
     render() {
         return (
             <div>
-                <div className="col-xs-6 col-md-4 col-lg-4">
-                    <Pie data={this.state.piedata} width="200%" height="200%"/>
+                <div className="row">
+                    <div className="text-left">
+                        <h3 className="dashboard-subheading">Previous Targets Analytics</h3>
+                        <hr></hr>
+                    </div>
                 </div>
-                <div className="col-xs-6 col-md-4 col-lg-4">
-                    <br/><br/>
-                    <ChartLegend datasets={this.state.piedata}/>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 vulchart">
+                        <Pie data={this.state.piedata} width="175%" height="175%"/>
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 vulchart-legend">
+                        <br/><br/>
+                        <ChartLegend datasets={this.state.piedata}/>
+                    </div>
                 </div>
             </div>
         );
