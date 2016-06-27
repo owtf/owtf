2eb863b90aa2e14f55cc4c8050fdafe08efb39c1
saganshul <sganshuliiith@gmail.com>
2016-06-28 01:29:35 +0530

    [webui] Javascript compression plugin added to webpack and gulpfile created for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,51 @@
-var config = {
-    entry: './includes/src/main.js',
+var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
 
-    output: {
-        path:'./includes/js/',
-        filename: 'bundle.js',
-    },
+var slash = require('slash');
+var dirname = __dirname;
+if (process.platform === 'win32') dirname = slash(dirname);
 
-    devServer: {
-        inline: true,
-        port: 7777
-    },
-
-    module: {
-        loaders: [ {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-
-            query: {
-                presets: ['es2015', 'react']
+var common = {
+    plugins: [
+        new webpack.DefinePlugin({
+            'TARGET': '"' + TARGET + '"'
+        }),
+        new webpack.DefinePlugin({
+            '__base': '"' + dirname + '/"'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
             }
+        }),
+    ],
+    output: {
+        path: path.join(__dirname, 'includes/js/'),
+        filename: 'bundle.js',
+        publicPath: 'includes/js/'
+    }
+};
+
+var config = merge(common, {
+    entry: [
+        './includes/src/main'
+    ],
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: true,
+            mangle: true
+        })
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loaders: ['babel', 'babel?presets[]=react,presets[]=es2015'],
+            include: path.join(__dirname, 'includes/src')
         }]
     }
-
-}
+});
 
 module.exports = config;
