b81daf319d11804ec9a4998139a9fb6c509b4c4e
saganshul <sganshuliiith@gmail.com>
2016-07-06 17:15:58 +0530

    [webui] Transaction page modified to full width

--- includes/src/Transactions/Transactions.js
+++ includes/src/Transactions/Transactions.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class Transactions extends React.Component {
+
+    /* Function which replace the container to full width */
+    replaceContainer() {
+        document.getElementById("app").parentElement.className = "container-fluid";
+    };
+
     render() {
+        this.replaceContainer.bind(this)();
         return (
             <div></div>
         );
