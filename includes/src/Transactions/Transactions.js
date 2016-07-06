410970de463e65bfd3276fddf22f0d62a024a3a3
saganshul <sganshuliiith@gmail.com>
2016-07-06 17:12:46 +0530

    [webui] Transaction page initialized

---
+++ includes/src/Transactions/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Transactions extends React.Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Transactions;
