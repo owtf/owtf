f395db37d08b6cf09f6f4d2f1ec1d226766ac623
saganshul <sganshuliiith@gmail.com>
2016-10-06 12:18:29 +0530

    [webui] Changes Rank button from radio input to actual buttons

--- includes/src/Report/RankButtons.js
+++ includes/src/Report/RankButtons.js
@@ -6,91 +6,79 @@ class RankButtons extends React.Component {
         var obj = this.props.obj;
         var user_rank = obj['user_rank'];
         var owtf_rank = obj['owtf_rank'];
-        if (user_rank in[0,
+        if (user_rank in [0,
             1,
             2,
             3,
             4,
             5]) {
             return (
-                <div>
-                    <label className={user_rank === 0
-                        ? "btn rank"
-                        : "btn rank active"} title="Passing" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                <div className="btn-group">
+                    <button type="button" className={user_rank === 0
+                        ? "btn active"
+                        : "btn"}>
                         <i className="fa fa-thumbs-up"></i>
-                    </label>
-                    <label className={user_rank === 1
-                        ? "btn rank"
-                        : "btn rank active btn-success"} title="Informational" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={user_rank === 1
+                        ? "btn active btn-success"
+                        : "btn"}>
                         <i className="fa fa-info-circle"></i>
-                    </label>
-                    <label className={user_rank === 2
-                        ? "btn rank"
-                        : "btn rank active btn-info"} title="Low" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={user_rank === 2
+                        ? "btn active btn-info"
+                        : "btn"}>
                         <i className="fa fa-exclamation-circle"></i>
-                    </label>
-                    <label className={user_rank === 3
-                        ? "btn rank"
-                        : "btn rank active btn-warning"} title="Medium" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
-                        <i className="fa fa-warning"></i>
-                    </label>
-                    <label className={user_rank === 4
-                        ? "btn rank"
-                        : "btn rank active btn-danger"} title="High" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={user_rank === 3
+                        ? "btn active btn-warning"
+                        : "btn"}>
+                        <i className="fa fa-thumbs-up"></i>
+                    </button>
+                    <button type="button" className={user_rank === 4
+                        ? "btn active btn-danger"
+                        : "btn"}>
                         <i className="fa fa-bell"></i>
-                    </label>
-                    <label className={user_rank === 5
-                        ? "btn rank"
-                        : "btn rank active btn-critical"} title="Critical" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={user_rank === 5
+                        ? "btn active btn-critical"
+                        : "btn"}>
                         <i className="fa fa-bomb"></i>
-                    </label>
+                    </button>
                 </div>
             );
         } else {
             return (
-                <div>
-                    <label className={user_rank === 0
-                        ? "btn rank"
-                        : "btn rank active"} title="Passing" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                <div className="btn-group">
+                    <button type="button" className={user_rank === 0
+                        ? "btn active"
+                        : "btn"}>
                         <i className="fa fa-thumbs-up"></i>
-                    </label>
-                    <label className={owtf_rank === 1
-                        ? "btn rank"
-                        : "btn rank active btn-success"} title="Informational" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={owtf_rank === 1
+                        ? "btn active btn-success"
+                        : "btn"}>
                         <i className="fa fa-info-circle"></i>
-                    </label>
-                    <label className={owtf_rank === 2
-                        ? "btn rank"
-                        : "btn rank active btn-info"} title="Low" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={owtf_rank === 2
+                        ? "btn active btn-info"
+                        : "btn"}>
                         <i className="fa fa-exclamation-circle"></i>
-                    </label>
-                    <label className={owtf_rank === 3
-                        ? "btn rank"
-                        : "btn rank active btn-warning"} title="Medium" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
-                        <i className="fa fa-warning"></i>
-                    </label>
-                    <label className={owtf_rank === 4
-                        ? "btn rank"
-                        : "btn rank active btn-danger"} title="High" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={owtf_rank === 3
+                        ? "btn active btn-warning"
+                        : "btn"}>
+                        <i className="fa fa-thumbs-up"></i>
+                    </button>
+                    <button type="button" className={owtf_rank === 4
+                        ? "btn active btn-danger"
+                        : "btn"}>
                         <i className="fa fa-bell"></i>
-                    </label>
-                    <label className={user_rank === 5
-                        ? "btn rank"
-                        : "btn rank active btn-critical"} title="Critical" data-code={obj['plugin_code']}>
-                        <input type="radio" name="user_rank_button"/>
+                    </button>
+                    <button type="button" className={user_rank === 5
+                        ? "btn active btn-critical"
+                        : "btn"}>
                         <i className="fa fa-bomb"></i>
-                    </label>
+                    </button>
                 </div>
             );
         }
