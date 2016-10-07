800bf19b744094229f1402ab931c73d053f0a399
saganshul <sganshuliiith@gmail.com>
2016-10-07 22:55:56 +0530

    [webui] Colors plugin types buttons on Accordian

--- includes/src/Report/Accordians.js
+++ includes/src/Report/Accordians.js
@@ -3,33 +3,41 @@ import Collapse from './Collapse'
 
 class Accordians extends React.Component {
 
+    getRank(pluginDataList) {
+        var testCaseMax = 0;
+        var maxUserRank = -1;
+        var maxOWTFRank = -1;
+        for (var i = 0; i < pluginDataList.length; i++) {
+            if (pluginDataList[i].user_rank != null && pluginDataList[i].user_rank != -1) {
+                if (pluginDataList[i].user_rank > maxUserRank) {
+                    maxUserRank = pluginDataList[i].user_rank;
+                }
+            } else if (pluginDataList[i].owtf_rank != null && pluginDataList[i].owtf_rank != -1) {
+                if (pluginDataList[i].owtf_rank > maxOWTFRank) {
+                    maxOWTFRank = pluginDataList[i].owtf_rank;
+                }
+            }
+        }
+        testCaseMax = (maxUserRank > maxOWTFRank)
+            ? maxUserRank
+            : maxOWTFRank;
+        return testCaseMax;
+    };
+
     render() {
         var plugins = this.context.pluginNameData;
         var pluginData = this.context.pluginData;
+        var getRank = this.getRank;
+
         return (
             <div className="panel-group">
                 {Object.keys(plugins).map(function(key) {
+                    if (key in pluginData) {
+                        var testCaseMax = getRank.call(this, pluginData[key]);
+                    }
                     return (
                         <div className={(() => {
                             if (key in pluginData) {
-                                var testCaseMax = 0;
-                                var maxUserRank = -1;
-                                var maxOWTFRank = -1;
-                                var pluginDataList = pluginData[key];
-                                for (var i = 0; i < pluginDataList.length; i++) {
-                                    if (pluginDataList[i].user_rank != null && pluginDataList[i].user_rank != -1) {
-                                        if (pluginDataList[i].user_rank > maxUserRank) {
-                                            maxUserRank = pluginDataList[i].user_rank;
-                                        }
-                                    } else if (pluginDataList[i].owtf_rank != null && pluginDataList[i].owtf_rank != -1) {
-                                        if (pluginDataList[i].owtf_rank > maxOWTFRank) {
-                                            maxOWTFRank = pluginDataList[i].owtf_rank;
-                                        }
-                                    }
-                                }
-                                testCaseMax = (maxUserRank > maxOWTFRank)
-                                    ? maxUserRank
-                                    : maxOWTFRank;
                                 if (testCaseMax == 0)
                                     return "panel panel-passing";
                                 else if (testCaseMax === 1)
@@ -43,8 +51,7 @@ class Accordians extends React.Component {
                                 else if (testCaseMax === 5)
                                     return "panel panel-critical";
                                 return "panel panel-default";
-                            }
-                            else {
+                            } else {
                                 return "panel panel-default";
                             }
                         })()} key={key}>
@@ -59,7 +66,25 @@ class Accordians extends React.Component {
                                                     <div className="btn-group btn-group-xs" role="group">
                                                         {pluginData[key].map(function(obj) {
                                                             return (
-                                                                <button key={key + obj['plugin_type'].split('_').join(' ')} className="plugin_type_acc btn" style={{
+                                                                <button key={key + obj['plugin_type'].split('_').join(' ')} className={(() => {
+                                                                    if (key in pluginData) {
+                                                                        if (testCaseMax == 0)
+                                                                            return "plugin_type_acc btn btn-default";
+                                                                        else if (testCaseMax === 1)
+                                                                            return "plugin_type_acc btn btn-success";
+                                                                        else if (testCaseMax === 2)
+                                                                            return "plugin_type_acc btn btn-info";
+                                                                        else if (testCaseMax === 3)
+                                                                            return "plugin_type_acc btn btn-warning";
+                                                                        else if (testCaseMax === 4)
+                                                                            return "plugin_type_acc btn btn-danger";
+                                                                        else if (testCaseMax === 5)
+                                                                            return "plugin_type_acc btn btn-critical";
+                                                                        return "plugin_type_acc btn";
+                                                                    } else {
+                                                                        return "plugin_type_acc btn";
+                                                                    }
+                                                                })()} style={{
                                                                     marginTop: "23px"
                                                                 }} type="button">{obj['plugin_type'].split('_').join(' ').charAt(0).toUpperCase() + obj['plugin_type'].split('_').join(' ').slice(1)}</button>
                                                             );
