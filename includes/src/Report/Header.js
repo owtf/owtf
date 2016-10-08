7299ce77b493949a564fb408484a1f764dc56708
saganshul <sganshuliiith@gmail.com>
2016-10-08 19:28:08 +0530

    [webui] Tags the present rank of the target at target report page

--- includes/src/Report/Header.js
+++ includes/src/Report/Header.js
@@ -2,8 +2,32 @@ import React from 'react';
 import {TARGET_UI_URI} from '../constants';
 
 class Header extends React.Component {
+    getLocalRank() {
+        var localMax = 0;
+        var maxUserRank = -1;
+        var maxOWTFRank = -1;
+        var pluginData = this.context.pluginData;
+        for (var key in pluginData) {
+            if (pluginData.hasOwnProperty(key)) {
+                var poutputs = pluginData[key];
+                for (var i = 0; i < poutputs.length; i++) {
+                    if (poutputs[i].user_rank != null && poutputs[i].user_rank > maxUserRank) {
+                        maxUserRank = poutputs[i].user_rank;
+                    } else if (poutputs[i].owtf_rank != null && poutputs[i].owtf_rank > maxOWTFRank) {
+                        maxOWTFRank = poutputs[i].owtf_rank;
+                    }
+                }
+            }
+        }
+
+        localMax = (maxUserRank > maxOWTFRank)
+            ? maxUserRank
+            : maxOWTFRank;
+        return localMax;
+    };
 
     render() {
+        var localMax = this.getLocalRank.call(this);
         return (
             <div>
                 <ul className="breadcrumb">
@@ -13,7 +37,7 @@ class Header extends React.Component {
                     <li>
                         <a href={TARGET_UI_URI}>Target</a>
                     </li>
-                    <li className="active">{this.props.targetdata.target_url}</li>
+                    <li className="active">{this.context.targetdata.target_url}</li>
                 </ul>
 
                 {/* Scroll to top */}
@@ -25,18 +49,32 @@ class Header extends React.Component {
                 <div className="col-md-10 col-md-offset-2">
                     <div className="page-header">
                         <div className="row">
-                            <div className="col-md-8">
+                            <div className="col-md-10">
                                 <h2 style={{
                                     wordWrap: "break-word"
                                 }}>
-                                    {this.props.targetdata.target_url}
+                                    {this.context.targetdata.target_url}
                                     <small>
-                                        {' (' + this.props.targetdata.host_ip + ')'}
+                                        {' (' + this.context.targetdata.host_ip + ')'}
                                     </small>
                                 </h2>
                             </div>
-                            <div className="col-md-4">
-                                <h3 id="rankingTargetInfo"></h3>
+                            <div className="col-md-2">
+                                <h3 dangerouslySetInnerHTML={(() => {
+                                    if (localMax == 0)
+                                        return {__html: "<i><small><label class='alert alert-passing' style='margin-bottom: 0px'>Passing</label></small></i>"};
+                                    else if (localMax == 1)
+                                        return {__html: "<i><small><label class='alert alert-success' style='margin-bottom: 0px'>Info</label></small></i>"};
+                                    else if (localMax == 2)
+                                        return {__html: "<i><small><label class='alert alert-info' style='margin-bottom: 0px'>Low</label></small></i>"};
+                                    else if (localMax == 3)
+                                        return {__html: "<i><small><label class='alert alert-warning' style='margin-bottom: 0px'>Medium</label></small></i>"};
+                                    else if (localMax == 4)
+                                        return {__html: "<i><small><label class='alert alert-danger' style='margin-bottom: 0px'>High</label></small></i>"};
+                                    else if (localMax == 5)
+                                        return {__html: "<i><small><label class='alert alert-critical' style='margin-bottom: 0px'>Critical</label></small></i>"};
+                                    return {__html: ""};
+                                })()}></h3>
                             </div>
                         </div>
                     </div>
@@ -45,5 +83,11 @@ class Header extends React.Component {
         );
     }
 }
+
+Header.contextTypes = {
+    targetdata: React.PropTypes.object,
+    pluginNameData: React.PropTypes.object,
+    pluginData: React.PropTypes.object
+};
 
 export default Header;
