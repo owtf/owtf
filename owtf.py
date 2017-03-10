b1e998389415f83d626b44038a4d645e2ed5c074
Viyat <viyat001@gmail.com>
2017-03-09 19:07:50 -0500

    [virtualenv] Force check for Python virtualenv before any OWTF imports

--- owtf.py
+++ owtf.py
@@ -10,15 +10,24 @@ import os
 import sys
 import logging
 
+# Checks if the script is running inside a virtualenv or not
+# Stolen from http://stackoverflow.com/questions/1871549/python-determine-if-running-inside-virtualenv
+# Inside a virtualenv, sys.prefix points to the virtualenv directory,
+# and sys.real_prefix points to the "real" prefix of the system Python (often /usr or /usr/local or some such).
+# Outside the virtualenv, sys.real_prefix does not exist.
+if not hasattr(sys, 'real_prefix'):
+    print("[-] It seems that virtualenv has not been activated!\n")
+    print("[-] Please run: source ~/.%src; workon owtf\n" % os.environ["SHELL"].split(os.sep)[-1])
+    sys.exit(1)
+
+
 from framework.dependency_check import verify_dependencies
 from framework.core import Core
 from framework.dependency_management.component_initialiser import ComponentInitialiser, DatabaseNotRunningException
 from framework.dependency_management.dependency_resolver import ServiceLocator
 from framework import update
-from framework.utils import check_if_virtualenv_python
 from framework.lib.cli_options import usage, parse_options, parse_update_options
 
-check_if_virtualenv_python()
 verify_dependencies(os.path.dirname(os.path.abspath(sys.argv[0])) or '.')
 
 
