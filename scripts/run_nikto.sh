4f1e90d796d0412b0ab35cb9185ad887b293e451
Tikam Singh Alma <timonalma81@gmail.com>
2017-07-27 04:40:56 -0700

    Update run_nikto.sh

--- scripts/run_nikto.sh
+++ scripts/run_nikto.sh
@@ -33,7 +33,7 @@ fi
 NIKTO_SSL=""
 SSL_CONNECTION_LINES=$(sleep 5 ; echo -e "^C" 2> /dev/null | openssl s_client -connect "$HOST_NAME":"$PORT" -brief 2>&1 | grep "ESTABLISHED" | wc -l )
 
-if [ "$SSL_CONNECTION_LINES" -gt 0 ]; then # SSL connection successful, procede with -ssl switch
+if [ "$SSL_CONNECTION_LINES" -gt 0 ]; then # SSL connection successful, proceed with -ssl switch
        NIKTO_SSL="-ssl"
 fi
 if [ "$SSL_CONNECTION_LINES" -lt 1 ]; then # nossl connection.
