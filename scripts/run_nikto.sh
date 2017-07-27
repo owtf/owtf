485ddfe7610b83aac7ef607782bae9908f838e44
Tikam Singh Alma <timonalma81@gmail.com>
2017-07-27 06:43:56 -0700

    Update run_nikto.sh

--- scripts/run_nikto.sh
+++ scripts/run_nikto.sh
@@ -30,14 +30,11 @@ if [ "$5" ] && [ "$5" != "$ip" ]; then
         NIKTO_NOLOOKUP="" #Host name passed: must look up
 fi
 
-NIKTO_SSL=""
+NIKTO_SSL="-nossl"
 SSL_CONNECTION_LINES=$(sleep 5 ; echo -e "^C" 2> /dev/null | openssl s_client -connect "$HOST_NAME":"$PORT" -brief 2>&1 | grep "ESTABLISHED" | wc -l )
 
 if [ "$SSL_CONNECTION_LINES" -gt 0 ]; then # SSL connection successful, proceed with -ssl switch
        NIKTO_SSL="-ssl"
-fi
-if [ "$SSL_CONNECTION_LINES" -lt 1 ]; then # nossl connection.
-         NIKTO_SSL="-nossl"
 fi
 
 # Temporary nikto config file
