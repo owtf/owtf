8c7303693f064c7213582620fec6bacab5b36e24
Anshul Singhal <sganshuliiith@gmail.com>
2017-09-24 01:55:20 +0530

    cherry pick new react app architecture from ui-break

---
+++ owtf/webapp/app/app.js
@@ -0,0 +1,82 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+// Needed for redux-saga es6 generator support
+import 'babel-polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { useScroll } from 'react-router-scroll';
+import 'semantic-ui-css/semantic.min.css';
+
+// Import root app
+import App from 'containers/App';
+
+// Import selector for `syncHistoryWithStore`
+import { makeSelectLocationState } from 'containers/App/selectors';
+
+// Load the favicon, the manifest.json file
+/* eslint-disable import/no-unresolved, import/extensions */
+import '!file-loader?name=[name].[ext]!./favicon.ico';
+import '!file-loader?name=[name].[ext]!./manifest.json';
+/* eslint-enable import/no-unresolved, import/extensions */
+
+import configureStore from './store';
+
+// Import root routes
+import createRoutes from './routes';
+
+// Create redux store with history
+// this uses the singleton browserHistory provided by react-router
+// Optionally, this could be changed to leverage a created history
+// e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
+const initialState = {};
+const store = configureStore(initialState, browserHistory);
+
+// Sync history and store, as the react-router-redux reducer
+// is under the non-default key ("routing"), selectLocationState
+// must be provided for resolving how to retrieve the "route" in the state
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: makeSelectLocationState(),
+});
+
+// Set up the router, wrapping all Routes in the App component
+const rootRoute = {
+  component: App,
+  childRoutes: createRoutes(store),
+};
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router
+        history={history}
+        routes={rootRoute}
+        render={
+          // Scroll to top when going to a new page, imitating default browser
+          // behaviour
+          applyRouterMiddleware(useScroll())
+        }
+      />
+    </Provider>,
+    document.getElementById('app')
+  );
+};
+
+// Initial render
+render();
+
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+}
---
+++ owtf/webapp/app/containers/NotFoundPage/index.js
@@ -0,0 +1,22 @@
+/**
+ * NotFoundPage
+ *
+ * This is the page we show when the user visits a url that doesn't have a route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+
+export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <h1>
+        This is NotFoundPage component!
+      </h1>
+    );
+  }
+}
