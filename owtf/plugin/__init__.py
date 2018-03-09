110f83460fe006c3ade956a9bda1b23173e1bea3
Viyat Bhalodia <viyat001@gmail.com>
2018-03-09 11:01:00 -0500

    chore(cleanup): Add Sphinx logs to gitignore, and eslintrc.js

--- owtf/plugin/__init__.py
+++ owtf/plugin/__init__.py
@@ -1,12 +1,4 @@
 """
-owtf
+owtf.plugin
 ~~~~~
 """
-from owtf.db.database import Session, get_db_engine
-
-
-__version__ = '2.3b'
-__release__ = 'MacOWTF'
-
-Session.configure(bind=get_db_engine())
-db = Session()
