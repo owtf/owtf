baea522e6b5c3acceb08d743ce75f4c6f734f034
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2019-04-02 23:21:59 -0300

    This fixes the following scenario: When user is installing owtf and doesn't have tornado installed in its own system, when importing __version__ on setup.py, it will try to load OWTFLogger, that uses tornado, then breaking the application.
    My fix just bypass that by handling the LoadModule error and installation goes fine.

--- owtf/__init__.py
+++ owtf/__init__.py
@@ -3,7 +3,11 @@ from __future__ import absolute_import, unicode_literals
 import re
 from typing import Any, NamedTuple
 
-from owtf.utils.logger import OWTFLogger
+try:
+    from owtf.utils.logger import OWTFLogger
+    OWTFLogger().enable_logging()
+except ModuleNotFoundError as e:
+    print(e)
 
 __version__ = "2.6.0"
 __homepage__ = "https://github.com/owtf/owtf"
@@ -24,6 +28,3 @@ _temp = re.match(r"(\d+)\.(\d+).(\d+)(\.(.+))?", __version__).groups()
 VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]))
 del _temp
 __all__ = []
-
-
-OWTFLogger().enable_logging()
