0ee0d15f31fc952e1e264c69e6383e4c46080a2d
spiderxm <mrigank.rkee@rediffmail.com>
2021-04-05 20:45:06 +0530

    Updating button in Welcome Page

--- owtf/webapp/src/containers/WelcomePage/index.js
+++ owtf/webapp/src/containers/WelcomePage/index.js
@@ -1,10 +1,10 @@
 /*
  * WelcomePage
  */
-import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Button } from 'react-bootstrap';
-
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Button } from "react-bootstrap";
+import "./style.scss";
 export default class WelcomePage extends React.Component {
   // Since state and props are static,
   // there's no need to re-render this component
@@ -18,10 +18,18 @@ export default class WelcomePage extends React.Component {
         <div className="container">
           <div className="jumbotron">
             <h1>Offensive Web Testing Framework!</h1>
-            <p>OWASP OWTF test is a project that aims to make security assessments as efficient as possible.</p>
-            <div className="row" style={{ display: 'flex', justifyContent: 'center' }}>
+            <p>
+              OWASP OWTF test is a project that aims to make security
+              assessments as efficient as possible.
+            </p>
+            <div
+              className="row"
+              style={{ display: "flex", justifyContent: "center" }}
+            >
               <LinkContainer to="/login">
-                <Button bsStyle="primary">Login</Button>
+                <Button bsStyle="primary" className={"btn"}>
+                  Login
+                </Button>
               </LinkContainer>
             </div>
           </div>
