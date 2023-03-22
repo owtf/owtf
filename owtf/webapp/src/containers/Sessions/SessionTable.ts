ccc8e1deeafb1a343699ded173be885d5e4da389
Viyat <viyatb@users.noreply.github.com>
2023-03-22 11:36:06 -0700

    fixing tests (#1218)

--- owtf/webapp/src/containers/Sessions/SessionTable.ts
+++ owtf/webapp/src/containers/Sessions/SessionTable.ts
@@ -1,19 +1,19 @@
 /*
-  * This component manages the session table
-  * Handles the session changing and deletion functionality
-  * Renders the list of session in the form of a table
-  */
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import { Pane, Table, Spinner, IconButton, Radio } from 'evergreen-ui';
-import { filter } from 'fuzzaldrin-plus';
+ * This component manages the session table
+ * Handles the session changing and deletion functionality
+ * Renders the list of session in the form of a table
+ */
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Pane, Table, Spinner, IconButton, Radio } from "evergreen-ui";
+import { filter } from "fuzzaldrin-plus";
 
-interface ISessionsTable{
-  loading: boolean,
-  error: any,
-  sessions: any,
-  onChangeSession: Function,
-  onDeleteSession: Function,
+interface ISessionsTable {
+  loading: boolean;
+  error: any;
+  sessions: any;
+  onChangeSession: Function;
+  onDeleteSession: Function;
 }
 
 export default function SessionsTable({
@@ -21,28 +21,25 @@ export default function SessionsTable({
   error,
   sessions,
   onChangeSession,
-  onDeleteSession,
+  onDeleteSession
 }: ISessionsTable) {
-  
-  const[searchQuery, setSearchQuery] = useState(""), //Filter for session name
-  
-  /**
-   * Function filter the sessions based on their name data field
-   * @param {array} sessions List of all sessions
-   */
-  const handleTableFilter = (sessions: any[]) => {
-    const searchQueryy = searchQuery.trim();
+  const [searchQuery, setSearchQuery] = useState(""), //Filter for session name
+    /**
+     * Function filter the sessions based on their name data field
+     * @param {array} sessions List of all sessions
+     */
+    handleTableFilter = (sessions: any[]) => {
+      const searchQueryy = searchQuery.trim();
 
-    // If the searchQuery is empty, return the profiles as is.
-    if (searchQueryy.length === 0) return sessions;
+      // If the searchQuery is empty, return the profiles as is.
+      if (searchQueryy.length === 0) return sessions;
 
-    return sessions.filter(session => {
-      // Use the filter from fuzzaldrin-plus to filter by name.
-      const result = filter([session.name], searchQueryy)
-      return result.length === 1;
-    })
-  }
-
+      return sessions.filter(session => {
+        // Use the filter from fuzzaldrin-plus to filter by name.
+        const result = filter([session.name], searchQueryy);
+        return result.length === 1;
+      });
+    };
 
   /**
    * Function updates the name filter query
@@ -50,33 +47,41 @@ export default function SessionsTable({
    */
   const handleFilterChange = (value: any) => {
     setSearchQuery(value);
-  }
+  };
 
   /**
    * Function handles the changing of sessions
    * @param {object} e radio onchange event
    * @param {object} session session corresponding the radio box
-  */
-  const handleRadioChange = (e: { target: { checked: any; }; }, session: { id: any; name: any; }) => {
+   */
+  const handleRadioChange = (
+    e: { target: { checked: any } },
+    session: { id: any; name: any }
+  ) => {
     if (e.target.checked) {
-      onChangeSession({id: session.id, name:session.name});
+      onChangeSession({ id: session.id, name: session.name });
     }
   };
 
-    if (loading) {
-      return (
-        <Pane display="flex" alignItems="center" justifyContent="center" height={400}>
-          <Spinner size={32}/>
-        </Pane>
-      );
-    }
+  if (loading) {
+    return (
+      <Pane
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height={400}
+      >
+        <Spinner size={32} />
+      </Pane>
+    );
+  }
 
-    if (error !== false) {
-      return "<p>Something went wrong, please try again!</p>";
-    }
+  if (error !== false) {
+    return "<p>Something went wrong, please try again!</p>";
+  }
 
-    if (sessions !== false) {
-      const items = handleTableFilter(sessions);
+  if (sessions !== false) {
+    const items = handleTableFilter(sessions);
 
     return (
       <Table border>
@@ -87,7 +92,7 @@ export default function SessionsTable({
             width={300}
             onChange={handleFilterChange}
             value={searchQuery}
-            placeholder='Session name'
+            placeholder="Session name"
           />
           <Table.TextHeaderCell flex="none" width={120}>
             Delete Session
@@ -103,12 +108,16 @@ export default function SessionsTable({
                   onChange={e => handleRadioChange(e, session)}
                 />
               </Table.Cell>
-              <Table.TextCell flex="none" width={300}>{session.name}</Table.TextCell>
+              <Table.TextCell flex="none" width={300}>
+                {session.name}
+              </Table.TextCell>
               <Table.Cell flex="none" width={120}>
                 <IconButton
                   icon="trash"
                   intent="danger"
-                  onClick={() => onDeleteSession({id: session.id, name:session.name})}
+                  onClick={() =>
+                    onDeleteSession({ id: session.id, name: session.name })
+                  }
                 />
               </Table.Cell>
             </Table.Row>
@@ -124,5 +133,5 @@ SessionsTable.propTypes = {
   error: PropTypes.any,
   sessions: PropTypes.any,
   onChangeSession: PropTypes.func,
-  onDeleteSession: PropTypes.func,
+  onDeleteSession: PropTypes.func
 };
--- owtf/webapp/src/containers/Transactions/index.tsx
+++ owtf/webapp/src/containers/Transactions/index.tsx
@@ -1,58 +1,58 @@
 /*
  * Transactions
  */
-import React, { useEffect, useState } from 'react';
-import {Pane} from 'evergreen-ui';
-import './style.scss';
-import TransactionTable from './TransactionTable.js';
-import TransactionHeader from './TransactionHeader.js';
-import TargetList from './TargetList.js';
+import React, { useEffect, useState } from "react";
+import { Pane } from "evergreen-ui";
+import "./style.scss";
+import TransactionTable from "./TransactionTable";
+import TransactionHeader from "./TransactionHeader";
+import TargetList from "./TargetList";
 
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import {
   makeSelectFetchError,
   makeSelectFetchLoading,
-  makeSelectFetchTargets,
-} from '../TargetsPage/selectors';
+  makeSelectFetchTargets
+} from "../TargetsPage/selectors";
 import {
   makeSelectTransactionsError,
   makeSelectTransactionsLoading,
-  makeSelectFetchTransactions,
-} from './selectors';
+  makeSelectFetchTransactions
+} from "./selectors";
 import {
   makeSelectTransactionError,
   makeSelectTransactionLoading,
-  makeSelectFetchTransaction,
-} from './selectors';
+  makeSelectFetchTransaction
+} from "./selectors";
 import {
   makeSelectFetchHrtResponse,
   makeSelectHrtResponseError,
-  makeSelectHrtResponseLoading,
-} from './selectors';
-import { loadTargets } from '../TargetsPage/actions';
-import {loadTransactions, loadTransaction, loadHrtResponse } from './actions';
+  makeSelectHrtResponseLoading
+} from "./selectors";
+import { loadTargets } from "../TargetsPage/actions";
+import { loadTransactions, loadTransaction, loadHrtResponse } from "./actions";
 
-interface ITransactions{
-  targetsLoading: boolean,
-  targetsError: object | boolean,
-  targets: Array<any> | boolean,
-  transactionsLoading: boolean,
-  transactionsError: object | boolean,
-  transactions: Array<any> | boolean,
-  transactionLoading: boolean,
-  transactionError: object | boolean,
-  transaction: object | boolean,
-  hrtResponseLoading: boolean,
-  hrtResponseError: object | boolean,
-  onFetchTargets: Function,
-  onFetchTransactions: Function,
-  onFetchTransaction: Function,
-  onFetchHrtResponse: Function,
+interface ITransactions {
+  targetsLoading: boolean;
+  targetsError: object | boolean;
+  targets: Array<any> | boolean;
+  transactionsLoading: boolean;
+  transactionsError: object | boolean;
+  transactions: Array<any> | boolean;
+  transactionLoading: boolean;
+  transactionError: object | boolean;
+  transaction: object | boolean;
+  hrtResponseLoading: boolean;
+  hrtResponseError: object | boolean;
+  onFetchTargets: Function;
+  onFetchTransactions: Function;
+  onFetchTransaction: Function;
+  onFetchHrtResponse: Function;
 }
 
-export function Transactions({  
+export function Transactions({
   targetsLoading,
   targetsError,
   targets,
@@ -67,8 +67,8 @@ export function Transactions({
   onFetchTargets,
   onFetchTransactions,
   onFetchTransaction,
-  onFetchHrtResponse,}: ITransactions) {
-
+  onFetchHrtResponse
+}: ITransactions) {
   const [resizeTableActiveLeft, setResizeTableActiveLeft] = useState(false);
   const [widthTargetList, setWidthTargetList] = useState(16.66666667);
   const [minWidthTargetList, setMinWidthTargetList] = useState(10);
@@ -76,13 +76,13 @@ export function Transactions({
   const [minWidthTable, setMinWidthTable] = useState(20);
   const [target_id, setTarget_id] = useState(0);
   const [transactionHeaderData, setTransactionHeaderData] = useState({
-    id: '',
-    requestHeader: '',
-    responseHeader: '',
-    responseBody: '',
+    id: "",
+    requestHeader: "",
+    responseHeader: "",
+    responseBody: ""
   });
   const [transactionsData, setTransactionsData] = useState([]);
-  const [hrtResponse, setHrtResponse] = useState('');
+  const [hrtResponse, setHrtResponse] = useState("");
   const [headerHeight, setHeaderHeight] = useState(0);
 
   /* Function responsible for filling transaction table */
@@ -92,14 +92,14 @@ export function Transactions({
       setTarget_id(target_id);
       setTransactionsData([]);
       setTransactionHeaderData({
-        id: '',
-        requestHeader: '',
-        responseHeader: '',
-        responseBody: '',
+        id: "",
+        requestHeader: "",
+        responseHeader: "",
+        responseBody: ""
       });
-      setHrtResponse('');
+      setHrtResponse("");
     }, 500);
-  }
+  };
 
   /* Function responsible for filling data TransactionHeaders and Body component */
   const getTransactionsHeaders = (target_id: any, transaction_id: any) => {
@@ -111,12 +111,12 @@ export function Transactions({
           id: transactionn.id,
           requestHeader: transactionn.raw_request,
           responseHeader: transactionn.response_headers,
-          responseBody: transactionn.response_body,
+          responseBody: transactionn.response_body
         });
-        setHrtResponse('');
+        setHrtResponse("");
       }
     }, 500);
-  }
+  };
 
   /* Function which is handling HRT (http request handler) for request translation.
      Rest API - POST /api/targets/<target_id>/transactions/hrt/<transaction_id/
@@ -130,16 +130,20 @@ export function Transactions({
         setHrtResponse(hrtResponse);
       }
     }, 500);
-  }
+  };
 
   useEffect(() => {
     onFetchTargets();
-    document.addEventListener('mousemove', (e) => handleMouseDragLeft(e));
-    document.addEventListener('mouseup', (e) => handleMouseUp(e));
+    document.addEventListener("mousemove", e => handleMouseDragLeft(e));
+    document.addEventListener("mouseup", e => handleMouseUp(e));
     return () => {
-      document.removeEventListener('mousemove', (e: MouseEvent) => handleMouseDragLeft(e))
-      document.removeEventListener('mouseup', (e: MouseEvent) => handleMouseUp(e))
-    }
+      document.removeEventListener("mousemove", (e: MouseEvent) =>
+        handleMouseDragLeft(e)
+      );
+      document.removeEventListener("mouseup", (e: MouseEvent) =>
+        handleMouseUp(e)
+      );
+    };
   }, []);
 
   const handleHeaderContainerHeight = (changedValue: number) => {
@@ -149,21 +153,21 @@ export function Transactions({
   /* Clear header data in TransactionHeader during filters in TransactionTable */
   const updateHeaderData = () => {
     setTransactionHeaderData({
-      id: '',
-      requestHeader: '',
-      responseHeader: '',
-      responseBody: ''
+      id: "",
+      requestHeader: "",
+      responseHeader: "",
+      responseBody: ""
     });
-    setHrtResponse('');
-  }
+    setHrtResponse("");
+  };
 
   const handleMouseDown = (e: MouseEvent) => {
     setResizeTableActiveLeft(true);
-  }
+  };
 
   const handleMouseUp = (e: MouseEvent) => {
     setResizeTableActiveLeft(false);
-  }
+  };
 
   const handleMouseDragLeft = (e: MouseEvent) => {
     if (!resizeTableActiveLeft) {
@@ -179,32 +183,32 @@ export function Transactions({
       setWidthTargetList(minWidthTargetList);
       setWidthTable(96 - minWidthTargetList);
     }
-  }
+  };
 
   const TransactionHeaderProps = {
     target_id: target_id,
     transactionHeaderData: transactionHeaderData,
     hrtResponse: hrtResponse,
     getHrtResponse: getHrtResponse,
-    headerHeight: headerHeight,
+    headerHeight: headerHeight
   };
   const TargetListProps = {
     targets: targets,
-    getTransactions: getTransactions,
+    getTransactions: getTransactions
   };
   const TransactionTableProps = {
     getTransactionsHeaders: getTransactionsHeaders,
     target_id: target_id,
     updateHeaderData: updateHeaderData,
     handleHeaderContainerHeight: handleHeaderContainerHeight,
-    transactions: transactionsData,
-  }
+    transactions: transactionsData
+  };
   return (
     <Pane display="flex" flexDirection="row" data-test="transactionsComponent">
       <Pane
         id="left_panel"
         style={{
-          width: widthTargetList.toString() + '%',
+          width: widthTargetList.toString() + "%"
         }}
       >
         <TargetList {...TargetListProps} />
@@ -218,18 +222,18 @@ export function Transactions({
         flexDirection="column"
         id="right_panel"
         style={{
-          width: widthTable.toString() + '%',
+          width: widthTable.toString() + "%"
         }}
       >
         <Pane>
-          {target_id !== 0
-            ? <TransactionTable {...TransactionTableProps} />
-            : null}
+          {target_id !== 0 ? (
+            <TransactionTable {...TransactionTableProps} />
+          ) : null}
         </Pane>
         <Pane>
-          {target_id !== 0
-            ? <TransactionHeader {...TransactionHeaderProps} />
-            : null}
+          {target_id !== 0 ? (
+            <TransactionHeader {...TransactionHeaderProps} />
+          ) : null}
         </Pane>
       </Pane>
     </Pane>
@@ -241,19 +245,19 @@ Transactions.propTypes = {
   targetsError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
   targets: PropTypes.oneOfType([
     PropTypes.array.isRequired,
-    PropTypes.bool.isRequired,
+    PropTypes.bool.isRequired
   ]),
   transactionsLoading: PropTypes.bool,
   transactionsError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
   transactions: PropTypes.oneOfType([
     PropTypes.array.isRequired,
-    PropTypes.bool.isRequired,
+    PropTypes.bool.isRequired
   ]),
   transactionLoading: PropTypes.bool,
   transactionError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
   transaction: PropTypes.oneOfType([
     PropTypes.object.isRequired,
-    PropTypes.bool.isRequired,
+    PropTypes.bool.isRequired
   ]),
   hrtResponseLoading: PropTypes.bool,
   hrtResponseError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
@@ -261,7 +265,7 @@ Transactions.propTypes = {
   onFetchTargets: PropTypes.func,
   onFetchTransactions: PropTypes.func,
   onFetchTransaction: PropTypes.func,
-  onFetchHrtResponse: PropTypes.func,
+  onFetchHrtResponse: PropTypes.func
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -279,19 +283,19 @@ const mapStateToProps = createStructuredSelector({
 
   hrtResponse: makeSelectFetchHrtResponse,
   hrtResponseLoading: makeSelectHrtResponseLoading,
-  hrtResponseError: makeSelectHrtResponseError,
+  hrtResponseError: makeSelectHrtResponseError
 });
 
 const mapDispatchToProps = (dispatch: Function) => {
   return {
     onFetchTargets: () => dispatch(loadTargets()),
-    onFetchTransactions: (target_id: number) => dispatch(loadTransactions(target_id)),
-    onFetchTransaction: (target_id: any, transaction_id: any) => dispatch(loadTransaction(target_id, transaction_id)),
-    onFetchHrtResponse: (target_id: any, transaction_id: any, data: any) => dispatch(loadHrtResponse(target_id, transaction_id, data)),
+    onFetchTransactions: (target_id: number) =>
+      dispatch(loadTransactions(target_id)),
+    onFetchTransaction: (target_id: any, transaction_id: any) =>
+      dispatch(loadTransaction(target_id, transaction_id)),
+    onFetchHrtResponse: (target_id: any, transaction_id: any, data: any) =>
+      dispatch(loadHrtResponse(target_id, transaction_id, data))
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Transactions);
+export default connect(mapStateToProps, mapDispatchToProps)(Transactions);
