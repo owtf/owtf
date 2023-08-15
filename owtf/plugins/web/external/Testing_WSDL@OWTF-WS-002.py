e610a0ea218a13e22e78f4e2c3138e993b4aa16c
Aviral Jain <54772821+ph1ne4s@users.noreply.github.com>
2023-08-16 01:38:10 +0530

    Update Testing_WSDL@OWTF-WS-002.py
    removed space

--- owtf/plugins/web/external/Testing_WSDL@OWTF-WS-002.py
+++ owtf/plugins/web/external/Testing_WSDL@OWTF-WS-002.py
@@ -7,4 +7,4 @@ DESCRIPTION = "Plugin to assist manual testing"
 def run(PluginInfo):
     resource = get_resources("ExternalWebServices")
     Content = plugin_helper.resource_linklist("Online Resources", resource)
-    return Content 
+    return Content
