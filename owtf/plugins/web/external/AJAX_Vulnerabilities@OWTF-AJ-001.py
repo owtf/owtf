636f0c9a7fd8bdedcbbc4b9f16feeba21599c337
Aviral Jain <54772821+ph1ne4s@users.noreply.github.com>
2023-08-25 00:02:51 +0530

    Gobuster dir, bbot sub-domain and AJAX (#1233)
    * AJAX_Vulnerabilities
    
    * subdomain_takeover
    
    * gobuster
    
    * active dis gobuster

--- owtf/plugins/web/external/AJAX_Vulnerabilities@OWTF-AJ-001.py
+++ owtf/plugins/web/external/AJAX_Vulnerabilities@OWTF-AJ-001.py
@@ -1,11 +1,13 @@
 """
 AJAX testing
 """
+from owtf.managers.resource import get_resources
 from owtf.plugin.helper import plugin_helper
 
-DESCRIPTION = "Plugin to assist manual testing"
+DESCRIPTION = "AJAX Plugin to assist manual testing"
 
 
 def run(PluginInfo):
-    Content = plugin_helper.HtmlString("Intended to show helpful info in the future")
+    resource = get_resources("ExternalAJAX")
+    Content = plugin_helper.resource_linklist("Online Resources", resource)
     return Content
