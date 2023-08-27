0401b7a368abf45938e4148682bed9103609f172
ph1ne4s <jainaviral2002@gmail.com>
2023-08-28 00:28:30 +0530

    GCP Brute

---
+++ owtf/plugins/web/active/Testing_for_Open_GCP_buckets@OWTF-CL-002.py
@@ -0,0 +1,22 @@
+"""
+ACTIVE Plugin for Testing for Open GCP Buckets(OWASP-CL-002)
+"""
+from owtf.managers.resource import get_resources
+from owtf.plugin.helper import plugin_helper
+
+DESCRIPTION = "GCPBucketBrute for Open GCP Buckets"
+
+
+def run(PluginInfo):
+    resource = get_resources("ActiveOpenGCPBuckets")
+
+    # GCPBrute works better when we use Second Level Domain]
+    domain = resource[0][1]
+    # Extract Second Level Domain
+    extract_sld = domain.rsplit(".", 1)
+    # Replace it in the resource
+    resource[0][1] = extract_sld[0]
+
+    Content = plugin_helper.resource_linklist("Online Resources", resource)
+
+    return Content
