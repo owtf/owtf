69f31259f4cdac78600ec1f588d6c309d0e1c573
Aviral Jain <jainaviral2002@gmail.com>
2024-03-08 13:08:29 +0530

    plugin for IMAP_SMTP injection check (#1230)
    * test
    
    * IMAP_SMTP injection
    
    * Update base.txt
    
    specified version of command-injection-tester
    
    * Update resources.cfg
    
    removed space
    
    * Update Testing_WSDL@OWTF-WS-002.py
    
    removed space
    
    ---------
    
    Co-authored-by: Viyat <viyatb@users.noreply.github.com>

--- owtf/plugins/web/external/IMAP_SMTP_Injection@OWTF-DV-011.py
+++ owtf/plugins/web/external/IMAP_SMTP_Injection@OWTF-DV-011.py
@@ -1,8 +1,12 @@
+"""
+ACTIVE Plugin for traightforward testing of email servers for the STARTTLS command injection vulnerability in SMTP, POP3, and IMAP using command-injection-tester.
+"""
+from owtf.managers.resource import get_resources
 from owtf.plugin.helper import plugin_helper
 
-DESCRIPTION = "Plugin to assist manual testing"
+DESCRIPTION = "Active Vulnerability Scanning of email servers for the STARTTLS command injection vulnerability in SMTP, POP3, and IMAP."
 
 
 def run(PluginInfo):
-    Content = plugin_helper.HtmlString("Intended to show helpful info in the future")
-    return Content
+    resource = get_resources("IMAP_SMTPinjection")
+    return plugin_helper.CommandDump("Test Command", "Output", resource, PluginInfo, [])
