d15d8367c2fcc3394344817f3e6120ff22eaf7d6
Piyush Gupta <piyushgupta140104@gmail.com>
2026-03-03 02:28:43 +0530

    added code

--- owtf/managers/worklist.py
+++ owtf/managers/worklist.py
@@ -31,9 +31,7 @@ def load_works(session, target_urls, options):
     """
     for target_url in target_urls:
         if target_url:
-            target = get_target_config_dicts(
-                session=session, filter_data={"target_url": target_url}
-            )
+            target = get_target_config_dicts(session=session, filter_data={"target_url": target_url})
             group = options["plugin_group"]
             if options["only_plugins"] is None:
                 # If the plugin group option is the default one (not specified by the user).
@@ -81,9 +79,7 @@ def worklist_generate_query(session, criteria=None, for_stats=False):
         if criteria.get("target_url", None):
             if isinstance(criteria.get("target_url"), list):
                 criteria["target_url"] = criteria["target_url"][0]
-            query = query.filter(
-                Target.target_url.like("%%{!s}%%".format(criteria["target_url"]))
-            )
+            query = query.filter(Target.target_url.like("%%{!s}%%".format(criteria["target_url"])))
         if criteria.get("type", None):
             if isinstance(criteria.get("type"), list):
                 criteria["type"] = criteria["type"][0]
@@ -91,9 +87,7 @@ def worklist_generate_query(session, criteria=None, for_stats=False):
         if criteria.get("group", None):
             if isinstance(criteria.get("group"), list):
                 criteria["group"] = criteria["group"][0]
-            query = query.filter(
-                Plugin.group.like("%%{!s}%%".format(criteria["group"]))
-            )
+            query = query.filter(Plugin.group.like("%%{!s}%%".format(criteria["group"])))
         if criteria.get("name", None):
             if isinstance(criteria.get("name"), list):
                 criteria["name"] = criteria["name"][0]
@@ -114,9 +108,7 @@ def worklist_generate_query(session, criteria=None, for_stats=False):
                     criteria["limit"] = criteria["limit"][0]
                 query = query.limit(int(criteria["limit"]))
     except ValueError:
-        raise exceptions.InvalidParameterType(
-            "Invalid parameter type for transaction db"
-        )
+        raise exceptions.InvalidParameterType("Invalid parameter type for transaction db")
     return query
 
 
@@ -163,7 +155,7 @@ def get_work_for_target(session, in_use_target_list):
     query = session.query(Work).filter_by(active=True).order_by(Work.id)
     if len(in_use_target_list) > 0:
         query = query.filter(not_(Work.target_id.in_(in_use_target_list)))
-    work_obj = query.first()
+    work_obj = query.with_for_update(skip_locked=True).first()
     if work_obj:
         # First get the worker dict and then delete
         work_dict = _derive_work_dict(work_obj)
@@ -195,9 +187,7 @@ def get_work(session, work_id):
     """
     work = session.query(Work).get(work_id)
     if work is None:
-        raise exceptions.InvalidWorkReference(
-            "No work with id {!s}".format(str(work_id))
-        )
+        raise exceptions.InvalidWorkReference("No work with id {!s}".format(str(work_id)))
     return _derive_work_dict(work)
 
 
@@ -224,9 +214,7 @@ def group_sort_order(plugin_list):
         "external": 4,
     }
     # reverse = True so that descending order is maintained
-    sorted_plugin_list = sorted(
-        plugin_list, key=lambda k: priority[k["type"]], reverse=True
-    )
+    sorted_plugin_list = sorted(plugin_list, key=lambda k: priority[k["type"]], reverse=True)
     return sorted_plugin_list
 
 
@@ -250,22 +238,10 @@ def add_work(session, target_list, plugin_list, force_overwrite=False):
     for target in target_list:
         for plugin in sorted_plugin_list:
             # Check if it already in worklist
-            if (
-                get_count(
-                    session.query(Work).filter_by(
-                        target_id=target["id"], plugin_key=plugin["key"]
-                    )
-                )
-                == 0
-            ):
+            if get_count(session.query(Work).filter_by(target_id=target["id"], plugin_key=plugin["key"])) == 0:
                 # Check if it is already run ;) before adding
-                is_run = plugin_already_run(
-                    session=session, plugin_info=plugin, target_id=target["id"]
-                )
-                if (
-                    (force_overwrite is True)
-                    or (force_overwrite is False and is_run is False)
-                ):
+                is_run = plugin_already_run(session=session, plugin_info=plugin, target_id=target["id"])
+                if (force_overwrite is True) or (force_overwrite is False and is_run is False):
                     # If force overwrite is true then plugin output has
                     # to be deleted first
                     if force_overwrite is True:
@@ -289,9 +265,7 @@ def remove_work(session, work_id):
     """
     work_obj = session.query(Work).get(work_id)
     if work_obj is None:
-        raise exceptions.InvalidWorkReference(
-            "No work with id {!s}".format(str(work_id))
-        )
+        raise exceptions.InvalidWorkReference("No work with id {!s}".format(str(work_id)))
     session.delete(work_obj)
     session.commit()
 
@@ -320,9 +294,7 @@ def patch_work(session, work_id, active=True):
     """
     work_obj = session.query(Work).get(work_id)
     if work_obj is None:
-        raise exceptions.InvalidWorkReference(
-            "No work with id {!s}".format(str(work_id))
-        )
+        raise exceptions.InvalidWorkReference("No work with id {!s}".format(str(work_id)))
     if active != work_obj.active:
         work_obj.active = active
         session.merge(work_obj)
